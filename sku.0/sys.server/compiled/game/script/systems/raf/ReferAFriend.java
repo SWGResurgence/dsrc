518cae5171940c51fc0ef25ddbbf9eb87c990f8d
Talisa Knight <talisaknight@gmail.com>
2021-09-24 13:11:09 -0500

    Fixed Compile Issues with Refer A Friend Script.

--- sku.0/sys.server/compiled/game/script/systems/raf/ReferAFriend.java
+++ sku.0/sys.server/compiled/game/script/systems/raf/ReferAFriend.java
@@ -15,19 +15,19 @@ public class ReferAFriend extends script.base_script {
     public int setReferrer(obj_id self, obj_id target, String params, float defaultTime) throws InterruptedException {
         if (hasObjVar(self, "raf.referred")) {
             sendSystemMessageTestingOnly(self, "You have already set another player as your referrer.");
-            return SCRIPT_CONTINUE
+            return SCRIPT_CONTINUE;
         }
-        if (hasObjVar(target, "raf.referred") && getObjVar(target, "raf.referred") == self) {
+        if (hasObjVar(target, "raf.referred") && getObjIdObjVar(target, "raf.referred") == self) {
             sendSystemMessageTestingOnly(self, "You cannot refer this friend because he is referred to you.");
-            return SCRIPT_CONTINUE
+            return SCRIPT_CONTINUE;
         }
-        if (hasObjVar(target, "raf.last_referred") && getObjVar(target, "raf.last_referred") >= getCurrentBirthDate()) {
+        if (hasObjVar(target, "raf.last_referred") && getIntObjVar(target, "raf.last_referred") >= getCurrentBirthDate()) {
             sendSystemMessageTestingOnly(self, "You cannot refer this friend because he has referred another friend within the last 24 hours.");
-            return SCRIPT_CONTINUE
+            return SCRIPT_CONTINUE;
         }
         if (getPlayerStationId(self) == getPlayerStationId(target)) {
             sendSystemMessageTestingOnly(self, "You cannot refer yourself.");
-            return SCRIPT_CONTINUE
+            return SCRIPT_CONTINUE;
         }
         obj_id[] items = new obj_id[2];
         items[0] = static_item.createNewItemFunction("item_reward_buddy_xp_chip_06_01", self);
@@ -40,7 +40,7 @@ public class ReferAFriend extends script.base_script {
         items[0] = static_item.createNewItemFunction("col_buddy_" + (rand(0, 1) == 0 ? "02_" : "") + "token", target);
         showLootBox(target, items);
         setObjVar(self, "raf.last_referred", getCurrentBirthDate());
-        sui.msgbox(target, target, "Congratulations! You have recieved a Buddy Token for referring player " + getPlayerName(self) + " to the game!", "Refer A Friend Rewards");
+        sui.msgbox(target, target, "Congratulations! You have recieved a Buddy Token for referring player " + getPlayerName(self) + " to the game!", "Refer a Friend Rewards");
         return SCRIPT_CONTINUE;
     }
 }
