785199b94bd5d46426b3cd50724eaa20d05cd342
Talisa Knight <talisaknight@gmail.com>
2021-09-24 17:29:10 -0500

    Increased Limit of Bounty Hunters Per Mission From 3 to 8.

--- sku.0/sys.server/compiled/game/script/library/bounty_hunter.java
+++ sku.0/sys.server/compiled/game/script/library/bounty_hunter.java
@@ -469,7 +469,7 @@ public class bounty_hunter extends script.base_script
         }
         int numHunters = hunters.length;
         String Smax = getConfigSetting("GameServer", "maxJediBounties");
-        int maxHunters = 3;
+        int maxHunters = 8;
         if (Smax != null && !Smax.equals(""))
         {
             Integer Imax = Integer.getInteger(Smax);
