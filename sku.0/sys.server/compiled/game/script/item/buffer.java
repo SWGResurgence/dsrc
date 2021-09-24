7d18b53bb6aeb795da5c1b5d5d24dd3b710943ac
Talisa Knight <talisaknight@gmail.com>
2021-09-24 17:40:17 -0500

    Changed String Name.

--- sku.0/sys.server/compiled/game/script/item/buffer.java
+++ sku.0/sys.server/compiled/game/script/item/buffer.java
@@ -4,15 +4,12 @@ import script.*;
 import script.library.buff;
 
 public class buffer extends script.base_script {
-
     public buffer() {
     }
-
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
-        mi.addRootMenu(menu_info_types.ITEM_USE, new string_id("sui", "use"));
+        mi.addRootMenu(menu_info_types.ITEM_USE, new string_id("sui", "apply_medic_buffs"));
         return SCRIPT_CONTINUE;
     }
-
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
 
         if(item == menu_info_types.ITEM_USE) {
@@ -23,7 +20,6 @@ public class buffer extends script.base_script {
 			buff.applyBuff(player,"me_buff_precision_3");
 			buff.applyBuff(player,"me_buff_melee_gb_1");
 			buff.applyBuff(player,"me_buff_ranged_gb_1");
-			
         }
         return SCRIPT_CONTINUE;
     }
