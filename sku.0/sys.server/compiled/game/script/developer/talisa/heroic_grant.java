1c667804530ecd38213b4eac5f79b05001322fdc
Talisa Knight <talisaknight@gmail.com>
2021-09-24 18:28:47 -0500

    Added Script to Grant Heroic Access.

---
+++ sku.0/sys.server/compiled/game/script/developer/talisa/heroic_grant.java
@@ -0,0 +1,52 @@
+package script.beta;
+
+import script.*;
+import script.library.instance;
+import script.library.resource;
+import script.library.sui;
+
+public class heroic_grant extends script.base_script {
+	public heroic_grant() {
+	}
+	public static final String HANDLER_SET_TOOL_CLASS = "handleSetToolClass";
+    public static final string_id SID_TOOL_OPTIONS = new string_id("sui", "Unlock_Heroics");
+    public static final string_id SID_TOOL_CLASS = new string_id("sui", "Set_Heroic");
+    public int OnInitialize(obj_id self) throws InterruptedException {
+		setName(self, "Heroic Unlock Terminal");
+        return SCRIPT_CONTINUE;
+	}
+	public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
+		if (item == menu_info_types.SERVER_SURVEY_TOOL_CLASS) {
+			String[] instanceFlags = dataTableGetStringColumn(instance.INSTANCE_DATATABLE, "key_required");
+            if (instanceFlags != null && instanceFlags.length > 0) {
+				for (String flag : instanceFlags) {
+					if (flag != null && flag.length() > 0) {
+						instance.flagPlayerForInstance(player, flag);
+					}
+				}
+				sendSystemMessageTestingOnly(player, "All Heroic Instances Flagged");
+			} else {
+				sendSystemMessageTestingOnly(player, "No Instance Flags Found.");
+			}
+		}
+		return SCRIPT_CONTINUE;
+	}
+	public int handleSetToolClass(obj_id self, dictionary params) throws InterruptedException {
+		obj_id player = params.getObjId("player");
+        int selRow = sui.getListboxSelectedRow(params);
+        switch (selRow) {
+			case -1:
+                return SCRIPT_CONTINUE;
+            default:
+                if ((player != null) && (player != obj_id.NULL_ID)) {
+					if (!resource.setToolClass(self, selRow)) {
+						sendSystemMessageTestingOnly(player, "Heroics Failed?");
+					} else {
+						sendSystemMessageTestingOnly(player, "Heroics!");
+					}
+				}
+				break;
+		}
+		return SCRIPT_CONTINUEl
+	}
+}
