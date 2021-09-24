3e633d61f1e4c8b5e496e64c84f695fec3cc60a6
Talisa Knight <talisaknight@gmail.com>
2021-09-24 17:58:36 -0500

    Fixed Bug with Med Buffer Script.

--- sku.0/sys.server/compiled/game/script/item/buffer_medic.java
+++ sku.0/sys.server/compiled/game/script/item/buffer_medic.java
@@ -3,23 +3,26 @@ package script.item;
 import script.*;
 import script.library.buff;
 
-public class buffer_medic extends script.base_script {
-    public buffer_medic() {
+public class med_buffer extends script.base_script {
+
+    public med_buffer() {
     }
+
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
         mi.addRootMenu(menu_info_types.ITEM_USE, new string_id("sui", "apply_medic_buffs"));
         return SCRIPT_CONTINUE;
     }
+
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
 
         if(item == menu_info_types.ITEM_USE) {
-            buff.applyBuff(player,"me_buff_health_2", 3600);
-			buff.applyBuff(player,"me_buff_action_3", 3600);
-			buff.applyBuff(player,"me_buff_strength_3", 3600);
-			buff.applyBuff(player,"me_buff_agility_3", 3600);
-			buff.applyBuff(player,"me_buff_precision_3", 3600);
-			buff.applyBuff(player,"me_buff_melee_gb_1", 3600);
-			buff.applyBuff(player,"me_buff_ranged_gb_1", 3600);
+            buff.applyBuff((player),"me_buff_health_2", 3600);
+			buff.applyBuff((player),"me_buff_action_3", 3600);
+			buff.applyBuff((player),"me_buff_strength_3", 3600);
+			buff.applyBuff((player),"me_buff_agility_3", 3600);
+			buff.applyBuff((player),"me_buff_precision_3", 3600);
+			buff.applyBuff((player),"me_buff_melee_gb_1", 3600);
+			buff.applyBuff((player),"me_buff_ranged_gb_1", 3600);
         }
         return SCRIPT_CONTINUE;
     }
