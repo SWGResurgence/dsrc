768cc6f1f69ccf0164f5cb11f37f7e3cd09f4e60
Talisa Knight <talisaknight@gmail.com>
2021-09-24 17:40:53 -0500

    Added One Hour Duration to Automated Medic Buff System.

--- sku.0/sys.server/compiled/game/script/item/buffer.java
+++ sku.0/sys.server/compiled/game/script/item/buffer.java
@@ -13,13 +13,13 @@ public class buffer extends script.base_script {
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
 
         if(item == menu_info_types.ITEM_USE) {
-            buff.applyBuff(player,"me_buff_health_2");
-			buff.applyBuff(player,"me_buff_action_3");
-			buff.applyBuff(player,"me_buff_strength_3");
-			buff.applyBuff(player,"me_buff_agility_3");
-			buff.applyBuff(player,"me_buff_precision_3");
-			buff.applyBuff(player,"me_buff_melee_gb_1");
-			buff.applyBuff(player,"me_buff_ranged_gb_1");
+            buff.applyBuff(player,"me_buff_health_2", 3600);
+			buff.applyBuff(player,"me_buff_action_3", 3600);
+			buff.applyBuff(player,"me_buff_strength_3", 3600);
+			buff.applyBuff(player,"me_buff_agility_3", 3600);
+			buff.applyBuff(player,"me_buff_precision_3", 3600);
+			buff.applyBuff(player,"me_buff_melee_gb_1", 3600);
+			buff.applyBuff(player,"me_buff_ranged_gb_1", 3600);
         }
         return SCRIPT_CONTINUE;
     }
