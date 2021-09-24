93bd4e6a7d9c32776faddb00f0d1b975479f8194
Talisa Knight <talisaknight@gmail.com>
2021-09-24 16:05:20 -0500

    Complete Rewrote and Retasked CuReward as a Publish Gift and Loyalty Reward System. Players Will Receive 100 Veteran Tokens, Plus 1 every 24 Hours. Revamped The GetVet Command to Print Out Remaining Cooldown.

--- sku.0/sys.server/compiled/game/script/cureward/cureward.java
+++ sku.0/sys.server/compiled/game/script/cureward/cureward.java
@@ -1,59 +1,96 @@
 package script.cureward;
 
-import script.dictionary;
+import script.base_script;
+import script.library.static_item;
 import script.obj_id;
 
-public class cureward extends script.base_script
-{
-    public cureward()
-    {
-    }
-    public int OnAttach(obj_id self) throws InterruptedException
-    {
-        if (getConfigSetting("GameServer", "combatUpgradeReward") == null)
-        {
-            return SCRIPT_CONTINUE;
-        }
-        if (!createRewards(self))
-        {
-            messageTo(self, "handleRetryRewardNextLogin", null, 1, false);
-        }
+public class cureward extends script.base_script {
+    private static final int DAYS_ON_LAUNCH = 6086;
+    private static final String[] OBJVARS = {
+        "loyalty_granted",
+        "gold_granted",
+        "respec_device_granted",
+        "bacta_tank_granted",
+        "publish28_granted",
+        "chapter1_granted",
+        "chapter2_granted",
+        "chapter3_granted",
+        "chapter4_granted",
+        "chapter5_granted",
+        "chapter6_granted",
+        "chapter7_granted",
+        "chapter8_granted",
+        "chapter9_granted",
+        "chapter10_granted",
+        "chapter11_granted",
+        "chapter12_granted",
+        "chapter13_granted",
+        "chapter14_granted"
+    };
+    private static final String[][] REWARDS = {
+        new String[]{ "recapture_gift_chapter_11_hoth_hologram_02_01", "object/tangible/furniture/decorative/hologram_nebulon_frigate.iff" },
+        new String[]{ "object/tangible/event_perk/frn_loyalty_award_plaque_silver.iff", "object/tangible/event_perk/frn_loyalty_award_plaque_gold.iff" },
+        new String[]{ "item_respec_token_01_01" },
+        new String[]{ "item_publish_gift_27_04_01", "weapon_publish_gift_27_04_01" },
+        new String[]{ "item_tcg_loot_reward_series4_ball_of_peace_02_01" },
+        new String[]{ "item_publish_gift_29_mustafar_04_01", "item_publish_gift_29_ryatt_04_01", "item_publish_gift_29_corellia_04_01" },
+        new String[]{ "item_gcw_base_reactive_heal_a_03_01" },
+        new String[]{ "item_gcw_recruitment_letter_01_01" },
+        new String[]{ "item_holopet_emitter_01_01", "item_holopet_data_cube_gift_01_01" },
+        new String[]{ "item_publish_gift_33_picture_01_01" },
+        new String[]{ "item_publish_gift_magic_control_object_03_01" },
+        new String[]{ "item_publish_gift_35_04_01" },
+        new String[]{ "item_publish_gift_36_04_01" },
+        new String[]{ "item_publish_gift_37_04_01" },
+        new String[]{ "meatlump_reward_rock_chair_02_01", "item_publish_gift_38_04_01" },
+        new String[]{ "publish_gift_chapter_11_snow_machine_02_01" },
+        new String[]{ "item_hologram_aotc_cybernetic_arm" },
+        new String[]{ "item_publish_gift_update_14_comlink" },
+        new String[]{ "item_publish_gift_update_14_statuette" }
+    };
+
+    public int OnAttach(obj_id self) throws InterruptedException {
         return SCRIPT_CONTINUE;
     }
-    public int OnInitialize(obj_id self) throws InterruptedException
-    {
-        if (getConfigSetting("GameServer", "combatUpgradeReward") == null)
-        {
-            detachScript(self, "cureward.cureward");
-        }
+
+    public int OnInitialize(obj_id self) throws InterruptedException {
+        int birth = getPlayerBirthDate(self) - DAYS_ON_LAUNCH;
+
+        if (birth <= 3)
+            CheckAndGrant(self, birth, 0);
+
+        if (birth <= 127 && !hasCommand(self, "veteranPlayerBuff"))
+            grantCommand(self, "veteranPlayerBuff");
+
+		obj_id tatooine = getPlanetByName("tatooine");
+		String objVar = "vetTokenCD_" + getStationId(self);
+		if (!hasObjVar(tatooine, objVar)) {
+			showLootBox(self, obj_id{ static_item.createNewItemFunction("item_vet_reward_token_01_01", self, 100) });
+			setObjVar(tatooine, objVar, getCalendarTime());
+		} else if (getCalendarTime() - getIntObjVar(tatooine, "vetTokenCD_" + getStationId(self)) >= 86400) {
+			showLootBox(self, obj_id{ static_item.createNewItemFunction("item_vet_reward_token_01_01", self) });
+			setObjVar(tatooine, objVar, getCalendarTime());
+		}
+
+        for (int i = 1; i < REWARDS.length; i++)
+            if (birth <= 7 * i)
+                CheckAndGrant(self, birth, i);
         return SCRIPT_CONTINUE;
     }
-    public boolean createRewards(obj_id self) throws InterruptedException
-    {
-        int bornOnDate = getPlayerBirthDate(self);
-        if (bornOnDate > 1579)
-        {
-            return true;
-        }
-        String cuRewardOption = getConfigSetting("GameServer", "combatUpgradeReward");
-        obj_id reward = createObjectInInventoryAllowOverload("object/tangible/event_perk/frn_loyalty_award_plaque_silver.iff", self);
-        if (!isIdValid(reward))
-        {
-            return false;
-        }
-        if (cuRewardOption.equals("2"))
-        {
-            reward = createObjectInInventoryAllowOverload("object/tangible/event_perk/frn_loyalty_award_plaque_gold.iff", self);
-            if (!isIdValid(reward))
-            {
-                return false;
+
+    private void CheckAndGrant(obj_id self, int birth, int tier) throws InterruptedException {
+        if (birth <= 7 * tier) {
+            if (!hasObjVar(self, "cureward." + OBJVARS[tier])) {
+                setObjVar(self, "cureward." + OBJVARS[tier], 1);
+                obj_id[] items = new obj_id[REWARDS[tier].length];
+                for (int i = 0; i < REWARDS[tier].length; i++) {
+                    if (REWARDS[tier][i].startsWith("object/"))
+                        items[i] = createObjectInInventoryAllowOverload(REWARDS[tier][i], self);
+                    else
+                        items[i] = static_item.createNewItemFunction(REWARDS[tier][i], self);
+                }
+                showLootBox(self, items);
             }
         }
-        return true;
-    }
-    public int handleRetryRewardNextLogin(obj_id self, dictionary params) throws InterruptedException
-    {
-        detachScript(self, "cureward.cureward");
-        return SCRIPT_CONTINUE;
     }
 }
