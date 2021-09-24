84a3a5813a545b5d993a78b8a131c860b9d63be9
Talisa Knight <talisaknight@gmail.com>
2021-09-24 17:36:15 -0500

    Fixed Issue Causing the Midlithe Crystals To Appear Twice on the Heroic Token Box.

--- sku.0/sys.server/compiled/game/script/library/trial.java
+++ sku.0/sys.server/compiled/game/script/library/trial.java
@@ -287,10 +287,9 @@ public class trial extends script.base_script
 		"item_nova_orion_space_resource_01_01",				//#29
 		"item_restuss_imperial_commendation_02_01",   		//#30
 		"item_restuss_rebel_commendation_02_01",			//#31
-		"item_nova_orion_space_resource_01_01",				//#32
-		"item_mustafar_reward_token_01_01",					//#33
+		"item_mustafar_reward_token_01_01",					//#32
     };
-    public static final int NUM_HEROIC_TOKEN_TYPES = 34;
+    public static final int NUM_HEROIC_TOKEN_TYPES = 33;
     public static final String KIMARU_HATE_LIST = "kimaru_hate_list";
     public static final String WP_DATA = "nearPoint";
     public static final String PROT_CHILD = "protected_data";
