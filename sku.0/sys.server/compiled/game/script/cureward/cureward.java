493ebfecd0fbfd8346daaf6f3189be91300e327f
Talisa Knight <talisaknight@gmail.com>
2021-09-25 04:45:19 -0500

    Fix Attempt.

--- sku.0/sys.server/compiled/game/script/cureward/cureward.java
+++ sku.0/sys.server/compiled/game/script/cureward/cureward.java
@@ -58,7 +58,7 @@ public class cureward extends script.base_script {
         return SCRIPT_CONTINUE;
     }
 
-    private static final int VET_TOKEN_BONUS = utils.getIntConfigSetting("GameServer", "veteranTokenBonus") throws InterruptedException {
+    private static final Byte VET_TOKEN_BONUS = utils.getByteConfigSetting("GameServer", "veteranTokenBonus") throws InterruptedException {
         return VET_TOKEN_BONUS;
 	}
 
