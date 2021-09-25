fa560f085d3a3fe477c2c68f7608412c96b88a84
Talisa Knight <talisaknight@gmail.com>
2021-09-24 23:48:47 -0500

    Update to Locked Containers.

--- sku.0/sys.server/compiled/game/script/structure/locked_container.java
+++ sku.0/sys.server/compiled/game/script/structure/locked_container.java
@@ -8,9 +8,6 @@ import java.util.Vector;
 
 public class locked_container extends script.base_script
 {
-    public locked_container()
-    {
-    }
     public static final string_id SID_LOCK_MENU = new string_id("spam", "sid_lock_menu");
     public static final string_id SID_MENU_REMOVE_LOCK = new string_id("spam", "sid_menu_lock_remove");
     public static final string_id SID_MENU_ADD_ACCESS_PLAYER = new string_id("spam", "sid_menu_lock_add_player");
@@ -127,7 +124,7 @@ public class locked_container extends script.base_script
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
     {
         obj_id lockedBy = getObjIdObjVar(self, "lock_owner");
-        if (isIdValid(lockedBy) && player == lockedBy)
+        if ((isIdValid(lockedBy) && player == lockedBy) || isGod(player))
         {
             int lockMenu = mi.addRootMenu(menu_info_types.SERVER_MENU1, SID_LOCK_MENU);
             mi.addSubMenu(lockMenu, menu_info_types.SERVER_MENU2, SID_MENU_REMOVE_LOCK);
