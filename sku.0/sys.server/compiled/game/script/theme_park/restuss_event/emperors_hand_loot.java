0ae8011af55db6e3b2ff935891aba53b57d0e054
Talisa Knight <talisaknight@gmail.com>
2021-09-26 10:55:27 -0500

    Added Missing Semi-Colon.

--- sku.0/sys.server/compiled/game/script/theme_park/restuss_event/emperors_hand_loot.java
+++ sku.0/sys.server/compiled/game/script/theme_park/restuss_event/emperors_hand_loot.java
@@ -38,7 +38,7 @@ public class emperors_hand_loot extends script.base_script {
 			static_item.createNewItemFunction("item_collection_sith_holocron_01_04", corpseInventory);
 			}
 		}
-		String myLoot1 = "item_restuss_schematic_saber_04_01"
+		String myLoot1 = "item_restuss_schematic_saber_04_01";
 		static_item.createNewItemFunction(myLoot1, corpseInventory, "");
 		return;
 	}
