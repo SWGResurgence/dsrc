ca8c97eaf49e745786a7a8d76ed2e402af8d64e5
Talisa Knight <talisaknight@gmail.com>
2021-09-26 10:53:23 -0500

    Added Loot Override for Emperor's Hand.

---
+++ sku.0/sys.server/compiled/game/script/theme_park/restuss_event/emperors_hand_loot.java
@@ -0,0 +1,46 @@
+package script.theme_park.restuss_event;
+
+import script.dictionary;
+import script.library.ai_lib;
+import script.library.static_item;
+import script.library.utils;
+import script.obj_id;
+
+public class emperors_hand_loot extends script.base_script {
+	public emperors_hand_loot() {
+	}
+	public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException {
+		obj_id corpseInventory = utils.
+		getInventoryContainer(self);
+		if (corpseInventory == null) {
+			return SCRIPT_CONTINUE;
+		}
+		if (!isIdValid(self)) {
+			return SCRIPT_CONTINUE;
+		}
+		createMyLoot(self);
+		return SCRIPT_CONTINUE;
+	}
+	public void createMyLoot(obj_id self) throws InterruptedException {
+		obj_id corpseInventory = utils.
+		getInventoryContainer(self);
+		if (corpseInventory == null) {
+			return;
+		}
+		String mobType = ai_lib.getCreatureName(self);
+		if (mobType == null) {
+			return;
+		}
+		int x = rand(1, 100);
+		if (x < 100) { /* 100% Drop Chance */
+			static_item.createNewItemFunction("rare_loot_chest_quality_3", corpseInventory);
+			if (x < 100) { /* 100% Drop Chance */
+			static_item.createNewItemFunction("item_collection_sith_holocron_01_04", corpseInventory);
+			}
+		}
+		String myLoot1 = "item_restuss_schematic_saber_04_01"
+		static_item.createNewItemFunction(myLoot1, corpseInventory, "");
+		return;
+	}
+}
+		
