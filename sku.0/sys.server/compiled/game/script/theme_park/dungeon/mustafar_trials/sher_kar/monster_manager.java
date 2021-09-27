814e7b5330e87cdc17e558c72b6f5cd9532f0c01
Talisa Knight <talisaknight@gmail.com>
2021-09-27 11:15:00 -0500

    More Compile Error Fixes.

--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/mustafar_trials/sher_kar/monster_manager.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/mustafar_trials/sher_kar/monster_manager.java
@@ -7,57 +7,61 @@ import script.obj_id;
 public class monster_manager extends script.base_script {
 	public monster_manager() {
 	}
-	public static final boolean LOGGING = false;
-	public int beginSpawn(obj_id self, dictionary params) throws InterruptedException {
-		clearEventArea(self);
-		messageTo(self, "prepareEventArea", null, 5, false);
-		return SCRIPT_CONTINUE;
-	}
-	public int dungeonCleanup(obj_id self, dictionary params) throws InterruptedException {
-		clearEventArea(self);
-		return SCRIPT_CONTINUE;
-	}
-	public int cleanupSpawn(obj_id self, dictionary params) throws InterruptedException {
-		obj_id[] content = trial.getAllObjectsInDungeon(dungeon);
-		if (content == null || contents.length == 0) {
-			doLogging("clearEventArea", "Dungeon was empty, return");
-			return;
-		}
-		for (obj_id content : contents) {
-			if (isPlayer(content)) {
-			} else {
-				if (isMob(content)) {
-					trial.cleanupNpc(content);
-				} else if (trial.isTempObject(content)) {
-					trial.cleanupNpc(content);
-				}
-			}
-		}
-	}
-	public int prepareEventArea(obj_id self, dictionary params) throws InterruptedException {
-		spawnSherKar(self);
-		spawnGuards(self);
-		return SCRIPT_CONTINUE;
-	}
-	public void spawnSherKar(obj_id self) throws InterruptedException {
-		obj_id[] wp = trial.getObjectsInDungeonWithObjVar(self, trial.CONSTER_WP);
-		if (wp == null || wp.length == 0) {
-			return;
-		}
-		for (obj_id obj_id : wp) {
-			if ((getStringObjVar(obj_id, trial.MONSTER_WP)).equals("sher_kar")) {
-				obj_id sherkar = create.object("som_sherkar", getLocation(obj_id));
-				setYaw(sherKar, -10);
-				utils.setScriptVar(self, trial.MONSTER_SHER_KAR, sherKar);
-			}
-		}
-	}
-	public void spawnGuards(obj_id self) throws InterruptedException {
-		obj_id[] wp = trial.getObjectsInDungeonWithObjVar(self, trial.MONSTER_WP);
-		if (wp == null || wp.length == 0) {
-			return;
-		}
-		int type = 0;
+public static final boolean LOGGING = false;
+    public int beginSpawn(obj_id self, dictionary params) throws InterruptedException {
+        clearEventArea(self);
+        messageTo(self, "prepareEventArea", null, 5, false);
+        return SCRIPT_CONTINUE;
+    }
+    public int dungeonCleanup(obj_id self, dictionary params) throws InterruptedException {
+        clearEventArea(self);
+        return SCRIPT_CONTINUE;
+    }
+    public int cleanupSpawn(obj_id self, dictionary params) throws InterruptedException {
+        clearEventArea(self);
+        return SCRIPT_CONTINUE;
+    }
+    public void clearEventArea(obj_id dungeon) throws InterruptedException {
+        obj_id[] contents = trial.getAllObjectsInDungeon(dungeon);
+        if (contents == null || contents.length == 0) {
+            doLogging("clearEventArea", "Dungeon was empty, return");
+            return;
+        }
+        for (obj_id content : contents) {
+            if (isPlayer(content)) {
+            } else {
+                if (isMob(content)) {
+                    trial.cleanupNpc(content);
+                } else if (trial.isTempObject(content)) {
+                    trial.cleanupNpc(content);
+                }
+            }
+        }
+    }
+    public int prepareEventArea(obj_id self, dictionary params) throws InterruptedException {
+        spawnSherKar(self);
+        spawnGuards(self);
+        return SCRIPT_CONTINUE;
+    }
+    public void spawnSherKar(obj_id self) throws InterruptedException {
+        obj_id[] wp = trial.getObjectsInDungeonWithObjVar(self, trial.MONSTER_WP);
+        if (wp == null || wp.length == 0) {
+            return;
+        }
+        for (obj_id obj_id : wp) {
+            if ((getStringObjVar(obj_id, trial.MONSTER_WP)).equals("sher_kar")) {
+                obj_id sherKar = create.object("som_sherkar", getLocation(obj_id));
+                setYaw(sherKar, -10);
+                utils.setScriptVar(self, trial.MONSTER_SHER_KAR, sherKar);
+            }
+        }
+    }
+    public void spawnGuards(obj_id self) throws InterruptedException {
+        obj_id[] wp = trial.getObjectsInDungeonWithObjVar(self, trial.MONSTER_WP);
+        if (wp == null || wp.length == 0) {
+            return;
+        }
+        int type = 0;
         obj_id[] guards = new obj_id[4];
         for (obj_id obj_id : wp) {
             if ((getStringObjVar(obj_id, trial.MONSTER_WP)).startsWith("guard")) {
@@ -76,22 +80,22 @@ public class monster_manager extends script.base_script {
                 }
             }
         }
-		ai_lib.establishAgroLink(guards[0], guards);
-	}
-	public int doMidEvent(obj_id self, dictionary params) throws InterruptedException {
-		obj_id[] players = trial.getValidTargetsInDungeon(self);
+        ai_lib.establishAgroLink(guards[0], guards);
+    }
+    public int doMidEvent(obj_id self, dictionary params) throws InterruptedException {
+        obj_id[] players = trial.getValidTargetsInDungeon(self);
         if (players == null || players.length == 0) {
-			return SCRIPT_CONTINUE;
-		}
-		return SCRIPT_CONTINUE;
-	}
-	public int doEndEvent(obj_id self, dictionary params) throws InterruptedException {
-		obj_id[] players = trial.getValidTargetsInDungeon(self);
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public int doEndEvent(obj_id self, dictionary params) throws InterruptedException {
+        obj_id[] players = trial.getValidTargetsInDungeon(self);
         if (players == null || players.length == 0) {
-			return SCRIPT_CONTINUE;
-		}
-		return SCRIPT_CONTINUE;
-	}
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
 	public int sherKarDied(obj_id self, dictionary params) throws InterruptedException {
 		utils.sendSystemMessagePob(self, trial.MONSTER_SK_DEFEATED);
 		obj_id[] players = trial.getPlayersInDungeon(self);
@@ -113,8 +117,8 @@ public class monster_manager extends script.base_script {
 	}
 	
 	public void doLogging(String section, String message) throws InterruptedException {
-		if (LOGGING || trial.MONSTER_LOGGING) {
-			LOG("doLogging/monster/monster_manager/" + section, message);
-		}
-	}
+        if (LOGGING || trial.MONSTER_LOGGING) {
+            LOG("doLogging/monster_manager/" + section, message);
+        }
+    }
 }
