1153eb825495d36e7c1113fd09f5cf1408dd18da
Talisa Knight <talisaknight@gmail.com>
2021-12-06 11:53:11 -0600

    Increased Veteran Reward Initial Amount from 100 to 500 Per Account.

--- sku.0/sys.server/compiled/game/script/player/base/base_player.java
+++ sku.0/sys.server/compiled/game/script/player/base/base_player.java
@@ -9577,7 +9577,7 @@ public class base_player extends script.base_script
 				obj_id tatooine = getPlanetByName("tatooine");
         String objVar = "vetTokenCD_" + getPlayerStationId(self);
 		if (!hasObjVar(tatooine, objVar)) {
-			cureward.giveVeteranRewardToken(self, 100);
+			cureward.giveVeteranRewardToken(self, 500);
 			return SCRIPT_CONTINUE;
 		}
         int timeLeft = getIntObjVar(tatooine, objVar) + 86400 - getCalendarTime();
