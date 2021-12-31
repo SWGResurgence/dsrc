6cad40ac6120b44887a6f3fc6e64a82afcf3b6e6
Talisa Knight <talisaknight@gmail.com>
2021-12-31 14:47:48 -0600

    Part of Previous Commit.

--- sku.0/sys.server/compiled/game/script/library/vet_token_stipend.java
+++ sku.0/sys.server/compiled/game/script/library/vet_token_stipend.java
@@ -17,7 +17,15 @@ public class vet_token_stipend extends script.base_script {
         obj_id tatooine = getPlanetByName("tatooine");
         String objVar = "vetToken_" + getPlayerStationId(player);
         if (!hasObjVar(tatooine, objVar)) {
-            if (!hasObjVar(tatooine, objVar) || (isGod(player))) {
+            /*if (!hasObjVar(tatooine, objVar) || (isGod(player))) {
+                if (item == menu_info_types.ITEM_USE) {
+                    sendSystemMessage(player, new string_id(STF_FILE, "opened_crate"));
+                    grantVetTokenStipend(self, player);
+                    return SCRIPT_CONTINUE;
+                }
+            }*/
+            
+            if (!hasObjVar(tatooine, objVar) {
                 if (item == menu_info_types.ITEM_USE) {
                     sendSystemMessage(player, new string_id(STF_FILE, "opened_crate"));
                     grantVetTokenStipend(self, player);
