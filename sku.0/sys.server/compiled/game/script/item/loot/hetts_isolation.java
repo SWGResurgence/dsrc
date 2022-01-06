8f78e9b8c3a30062e6c62b722fd41ac8bbe85408
Talisa Knight <talisaknight@gmail.com>
2022-01-06 12:40:07 -0600

    Fixed Script Errors.

--- sku.0/sys.server/compiled/game/script/item/loot/hetts_isolation.java
+++ sku.0/sys.server/compiled/game/script/item/loot/hetts_isolation.java
@@ -5,18 +5,12 @@ import script.library.static_item;
 import script.library.utils;
 import script.obj_id;
 
-public class hetts_isolation extends script.base_script
-{
-    public hetts_isolation()
-    {
-    }
+public class hetts_isolation extends script.base_script {
     private static final int HETTS_ISOLATION_LOOT_CHANCE = 1;
 
-    public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException
-    {
+    public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException {
         int chanceLoot = rand(1, 100);
-        if (chanceLoot > HETTS_ISOLATION_LOOT_CHANCE)
-        {
+        if (chanceLoot > HETTS_ISOLATION_LOOT_CHANCE) {
             return SCRIPT_CONTINUE;
         }
         static_item.createNewItemFunction("item_color_crystal_02_35", utils.getInventoryContainer(self));
