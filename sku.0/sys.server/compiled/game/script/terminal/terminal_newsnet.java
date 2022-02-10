0da63845db8048a14a566238a62788b687e6df7e
Talisa Knight <talisaknight@gmail.com>
2022-02-10 16:12:06 -0600

    Added New Scripting for the Newsnet Terminals.

---
+++ sku.0/sys.server/compiled/game/script/terminal/terminal_newsnet.java
@@ -0,0 +1,17 @@
+package script.terminal;
+
+import script.*;
+import gcw.*;
+
+public class terminal_newsnet extends script.base_script {
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info item) throws InterruptedException {
+        item.addRootMenu(menu_info_types.ITEM_USE, new string_id("gcw", "vote"));
+        return SCRIPT_CONTINUE;
+    }
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
+        if (item == menu_info_types.ITEM_USE) {
+            launchClientWebBrowser(player, "https://swgresurgence.com/forums/showthread.php?119-Game-Update-2-0-0");
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
