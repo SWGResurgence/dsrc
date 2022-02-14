a37a7ca119742fdd144bf41f8e6c483040d2cd12
SWG Rat <swg.xratx@gmail.com>
2022-02-14 19:34:51 +0000

    razor_angler_dna.java edited online with Bitbucket

--- sku.0/sys.server/compiled/game/script/item/dna/razor_angler_dna.java
+++ sku.0/sys.server/compiled/game/script/item/dna/razor_angler_dna.java
@@ -11,7 +11,7 @@ public class razor_angler_dna extends script.base_script
     {
     }
     public static final String RAZOR_ANGLER_DNA_LOOT_ITEM = "item_razor_angler_dna";
-    public static final int RAZOR_ANGLER_DNA_LOOT_CHANCE = 15;
+    public static final int RAZOR_ANGLER_DNA_LOOT_CHANCE = 5;
 
 
     public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException
