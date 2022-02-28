072ff6cbe9b27e34d9be4a46b41bee882c6e874f
Talisa Knight <talisaknight@gmail.com>
2022-02-28 15:42:32 -0600

    Part of Previous Commit.

--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/myyydril/grievous_encounter_manager.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/myyydril/grievous_encounter_manager.java
@@ -207,24 +207,6 @@ public class grievous_encounter_manager extends script.base_script
             messageTo(self, "handleEndEncounter", params, 60.0f, false);
         }
         
-        // HEROIC SYSTEM BEGIN \\
-        
-        dictionary dict = new dictionary();
-        dict.put("tokenIndex", 38);
-        dict.put("tokenCount", 5);
-        utils.messageTo(players, "handleAwardToken", dict, 0, false);
-        obj_id group = getGroupObject(players[0]);
-        int calendarTime = getCalendarTime();
-        String realTime = getCalendarTimeStringLocal(calendarTime);
-        CustomerServiceLog("instance-necrosis", "N-K Necrosis Defeated in instance (" + self + ") by group_id (" + group + ") at " + realTime);
-        CustomerServiceLog("instance-necrosis", "Group (" + group + ") consists of: ");
-        for (int i = 0; i < players.length; ++i) {
-            String strProfession = skill.getProfessionName(getSkillTemplate(players[i]));
-            CustomerServiceLog("instance-necrosis", "Group (" + group + ") member " + i + " " + getFirstName(players[i]) + "'s(" + players[i] + ") profession is " + strProfession + ".");
-        }
-        
-        // HEROIC SYSTEM END \\
-        
         return SCRIPT_CONTINUE;
     }
     public obj_id[] getEventPlayersInDungeon(obj_id dungeon) throws InterruptedException
