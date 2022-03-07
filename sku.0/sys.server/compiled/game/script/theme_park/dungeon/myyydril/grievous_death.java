18e7c7cc8f0523d4b81fb2c23114b66f3d27587c
Talisa Knight <talisaknight@gmail.com>
2022-03-06 19:44:35 -0600

    Part of Previous Commit.

--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/myyydril/grievous_death.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/myyydril/grievous_death.java
@@ -2,7 +2,6 @@ package script.theme_park.dungeon.myyydril;
 
 import script.dictionary;
 
-import script.library.*;
 import script.library.ai_lib;
 import script.library.static_item;
 import script.library.utils;
@@ -20,25 +19,6 @@ public class grievous_death extends script.base_script {
             return SCRIPT_CONTINUE;
         }
         createMyLoot(self);
-        
-        // HEROIC SYSTEM BEGIN \\
-        
-        dictionary dict = new dictionary();
-        dict.put("tokenIndex", 38);
-        dict.put("tokenCount", 5);
-        utils.messageTo(players, "handleAwardtoken", dict, 0, false);
-        obj_id group = getGroupObject(players[0]);
-        int calendarTime = getCalendarTime();
-        String realTime = getCalendarTimeStringLocal(calendarTime);
-        CustomerServiceLog("instance-necrosis", "N-K Necrosis Defeated in instance (" + self + ") by group_id (" + group + ") at " + realTime);
-        CustomerServiceLog("instance-necrosis", "Group (" + group + ") consists of: ");
-        for (int i = 0; i < players.length; ++i) {
-            String strProfession = skill.getProfessionName(getSkillTemplate(players[i]));
-            CustomerServiceLog("instance-necrosis", "Group (" + group + ") member " + i + " " + getFirstName(players[i]) + "'s(" + players[i] + ") profession is " + strProfession + ".");
-        }
-        
-        // HEROIC SYSTEM END \\
-        
         return SCRIPT_CONTINUE;
     }
     public void createMyLoot(obj_id self) throws InterruptedException {
@@ -52,12 +32,13 @@ public class grievous_death extends script.base_script {
         }
         int x = rand(1, 100);
         if(x < 31){  // 30% Chance to Drop General Grevious Painting
-           static_item.createNewItemFunction("item_general_grevious_painting_01_01", corpseInventory);
-      if(x < 15){  // 14% chance at dropping bonus loot (at least a Bane's Heart crystal) 
-          static_item.createNewItemFunction("item_color_crystal_02_16", corpseInventory);
-			if(x < 3){ // 2% chance to drop Grievous Gutsack
-          static_item.createNewItemFunction("item_tcg_loot_reward_series3_general_grievous_gutsack", corpseInventory);
-			}
+            static_item.createNewItemFunction("item_general_grevious_painting_01_01", corpseInventory);
+            if(x < 15){  // 14% chance at dropping bonus loot (at least a Bane's Heart crystal) 
+                static_item.createNewItemFunction("item_color_crystal_02_16", corpseInventory);
+            }
+            if(x < 3){ // 2% chance to drop Grievous Gutsack
+                static_item.createNewItemFunction("item_tcg_loot_reward_series3_general_grievous_gutsack", corpseInventory);
+            }
         }
         String myLoot1 = "object/tangible/ship/crafted/chassis/grievous_starfighter_reward_deed.iff";
         String myLoot2 = "object/tangible/wearables/cybernetic/s02/cybernetic_s02_arm_r.iff";
