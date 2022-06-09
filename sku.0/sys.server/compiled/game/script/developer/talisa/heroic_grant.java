bb3715b30eba067478b2c41aae22de55a30d950b
Talisa Knight <talisaknight@gmail.com>
2022-06-08 23:20:09 -0500

    Revert "Minor Bug Fix for Heroic Unlock."
    This reverts commit 8d9464ba5cf4a6e7a5b2161b153841de7eff071f.

--- sku.0/sys.server/compiled/game/script/developer/talisa/heroic_grant.java
+++ sku.0/sys.server/compiled/game/script/developer/talisa/heroic_grant.java
@@ -4,7 +4,6 @@ import script.*;
 import script.library.instance;
 import script.library.resource;
 import script.library.sui;
-import script.library.utils;
 
 public class heroic_grant extends script.base_script {
 	public heroic_grant() {
