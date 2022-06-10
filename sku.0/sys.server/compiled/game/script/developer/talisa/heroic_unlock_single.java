0b65675f52a398431b2c63c1cd0bda5a7f2b6305
Talisa Knight <talisaknight@gmail.com>
2022-06-09 21:46:10 -0500

    Added Functions for Heroic Unlock.

--- sku.0/sys.server/compiled/game/script/developer/talisa/heroic_unlock_single.java
+++ sku.0/sys.server/compiled/game/script/developer/talisa/heroic_unlock_single.java
@@ -1,39 +1,96 @@
 package script.developer.talisa;
 
 import script.*;
-import script.library.instance;
-import script.library.resource;
-import script.library.sui;
-import script.library.utils;
+import script.library.*;
+
+import java.util.ArrayList;
 
 public class heroic_unlock_single extends script.base_script {
     public heroic_unlock_single() {
     }
     
+    // Localized String for "use" taken from "sui.stf"
+    
     public static final string_id SID = new string_id("sui", "use");
+    
+    // Server Call to obtain menu
+    
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
-        if (!utils.isNestedWithin(self, player)) {
-            sendSystemMessageTestingOnly(player, "The Development Testing for Menu Request is currently disabled.");
-            return SCRIPT_CONTINUE;
-        }
-        menu_info_data mid = mi.getMenuItemByType(menu_info_types.ITEM_USE);
-        if (mid != null) {
-            mid.setServerNotify(true);
-            sendSystemMessageTestingOnly(player, "The Development Testing for Item Use is currently disabled.");
-        }
+        mi.addRootMenu(menu_info_types.ITEM_USE, SID);
+        sendSystemMessageTestingOnly(player, "Server Calling Menu.");
         return SCRIPT_CONTINUE;
     }
+    
+    // Server Call When Menu Item is Selected
     
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
         if (item == menu_info_types.ITEM_USE) {
             use(self, player);
-            sendSystemMessageTestingOnly(player, "The Development Testing for Menu Select is currently disabled.");
+            sendSystemMessageTestingOnly(player, "Server Calling Menu Select.");
             return SCRIPT_CONTINUE;
         }
         return SCRIPT_CONTINUE;
     }
     
+    // Server Call for Selected Menu Operation
+    
     public void use(obj_id self, obj_id player) throws InterruptedException {
-        //unlock heroics here...
+        String[] instanceFlags = dataTableGetStringColumn(instance.INSTANCE_DATATABLE, "key_required");
+        ArrayList<String> unflaggedInstances = new ArrayList<String>();
+        ArrayList<prose_package> instanceNames = new ArrayList<prose_package>();
+        cleanScriptVars(player);
+        
+        for (String instanceKey : instanceFlags) {
+            if (!instanceKey.equals("none") && !instance.isFlaggedForInstance(player, instanceKey)) {
+                unflaggedInstances.add(instanceKey);
+                prose_package pp = new prose_package();
+                pp.stringId = new string_id("instance", instanceKey);
+                pp.actor.set(player);
+                instanceNames.add(pp);
+            }
+        }
+        
+        if (unflaggedInstances.size() > 0) {
+            utils.setScriptVar(player, "heroic_unlock_single.unflaggedInstances", unflaggedInstances.toArray(new String[unflaggedInstances.size()]));
+            prose_package[] prose = instanceNames.toArray(new prose_package[instanceNames.size()]);
+            sui.listbox(self, player, "Select Heroic Instance", sui.OK_CANCEL,
+                "Available Instances",
+                prose,
+                "handleHeroicSelect", true, false);
+                
+        }else{
+            sendSystemMessageTestingOnly(player, "No Instancing To Flag");
+        }
+    }
+    
+    public int handleHeroicSelect(obj_id self, dictionary params) throws InterruptedException {
+        if (params == null || params.isEmpty()) {
+            return SCRIPT_CONTINUE;
+        }
+        
+        obj_id player = sui.getPlayerId(params);
+        int btn = sui.getIntButtonPressed(params);
+        int idx = sui.getListboxSelectedRow(params);
+        
+        if (btn == sui.BP_CANCEL) {
+            cleanScriptVars(player);
+            return SCRIPT_CONTINUE;
+        }
+        
+        if (utils.hasScriptVar(player, "heroic_unlock_single.unflaggedInstances")) {
+            String[] unflaggedInstances = utils.getStringArrayScriptVar(player, "heroic_unlock_single.unflaggedInstances");
+            if (idx == -1 || idx > unflaggedInstances.length) {
+                cleanScriptVars(player);
+                return SCRIPT_CONTINUE;
+            }
+            String instanceToFlag = unflaggedInstances[idx];
+            instance.flagPlayerForInstance(player, instanceToFlag);
+        }
+        
+        return SCRIPT_CONTINUE;
+    }
+    
+    public void cleanScriptVars(obj_id player) throws InterruptedException {
+        utils.removeScriptVarTree(player, "heroic_unlock_single");
     }
 }
