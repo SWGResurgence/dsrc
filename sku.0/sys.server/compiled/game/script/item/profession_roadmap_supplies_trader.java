d1fff0254e6f4c65841c17bc9329506d0a12ab29
Talisa Knight <talisaknight@gmail.com>
2022-06-12 22:55:05 -0500

    Added Scripting for Each Profession Roadmap Items.

---
+++ sku.0/sys.server/compiled/game/script/item/profession_roadmap_supplies_trader.java
@@ -0,0 +1,38 @@
+package script.item;
+
+import script.*;
+import java.util.HashSet;
+
+public class profession_roadmap_supplies_trader extends script.base_script {
+    public static final String STF_FILE = "npe";
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
+        int mnu2 = mi.addRootMenu(menu_info_types.ITEM_USE, new string_id(STF_FILE, "claim_contents"));
+        return SCRIPT_CONTINUE;
+    }
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
+        if (item == menu_info_types.ITEM_USE) {
+            sendSystemMessage(player, new string_id(STF_FILE, "opened_crate"));
+            obj_id[] allTheArmor = grantTraderRoadmapSupplies(player);
+            destroyObject(self);
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public static obj_id[] grantTraderRoadmapSupplies(obj_id player) throws InterruptedException {
+        obj_id pInv = utils.getInventoryContainer(player);
+        HashSet theSet = new HashSet();
+        theSet.add(static_item.createNewItemFunction("item_trader_backpack_01_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_trader_roadmap_jumpsuit_02_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_trader_roadmap_boots_02_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_trader_roadmap_gloves_02_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_trader_ring_01_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_trader_pendant_01_02", pInv));
+        theSet.add(static_item.createNewItemFunction("item_roadmap_belt_trader_01_02", pInv));
+        theSet.add(static_item.createNewItemFunction("weapon_knife_trader_roadmap_01_02", pInv));
+        theSet.add(static_item.createNewItemFunction("weapon_pistol_trader_roadmap_01_02", pInv));
+        obj_id[] items = new obj_id[theSet.size()];
+        theSet.toArray(items);
+        showLootBox(player, items);
+        return items;
+    }
+}
