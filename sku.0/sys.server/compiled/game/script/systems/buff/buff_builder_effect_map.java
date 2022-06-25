b78374ff64927b453db623538a409e57b9fd5750
Talisa Knight <talisaknight@gmail.com>
2022-06-25 11:30:58 -0500

    Rewrote Build A Buff Effect Mapping Script.

--- sku.0/sys.server/compiled/game/script/systems/buff/buff_builder_effect_map.java
+++ sku.0/sys.server/compiled/game/script/systems/buff/buff_builder_effect_map.java
@@ -5,34 +5,28 @@ import script.library.*;
 
 import java.util.ArrayList;
 
-public class buff_builder_effect_map extends script.base_script
-{
+public class buff_builder_effect_map extends script.base_script {
     public static final String DATATABLE_BUFF_BUILDER_EFFECT_MAP = "datatables/buff/buff_builder_effect_map.iff";
-    public static String[] getEffectList(String affectName) throws InterruptedException
-    {
-        //get the effect from the list
+    public buff_builder_effect_map() {
+    }
+    public static String[] getEffectList(String affectName) throws InterruptedException {
         ArrayList<String> effectList = new ArrayList<String>();
         int rowIndex = dataTableSearchColumnForString(affectName, "BUFF_BUILDER_AFFECTS", DATATABLE_BUFF_BUILDER_EFFECT_MAP);
-        if (rowIndex >= 0)
-        {
+        if (rowIndex >= 0) {
             int rowCount = dataTableGetNumRows(DATATABLE_BUFF_BUILDER_EFFECT_MAP);
             dictionary row;
             String affect;
-            for (int index = 0; index < rowCount; index++)
-            {
-                row = dataTableGetRow(DATATABLE_BUFF_BUILDER_EFFECT_MAP, index);
+            for (int index=0; index < rowCount; index++ {
+                row = dataTableGetNumRow(DATATABLE_BUFF_BUILDER_EFFECT_MAP, index);
                 affect = row.getString("BUFF_BUILDER_AFFECTS");
-
-                if (affect.equals(affectName))
-                {
+                if (affect.equals(affectName)) {
                     effectList.add(row.getString("AFFECT"));
                 }
             }
         }
         return effectList.toArray(new String[effectList.size()]);
     }
-    public static String[] getEffects() throws InterruptedException
-    {
+    public static String[] getEffects() throw InterruptedException {
         return dataTableGetStringColumn(DATATABLE_BUFF_BUILDER_EFFECT_MAP, "AFFECT");
     }
 }
