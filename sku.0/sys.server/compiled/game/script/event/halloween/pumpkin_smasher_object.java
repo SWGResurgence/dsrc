89243929ca357f239f28e655a42fb62b8f28f114
BubbaJoe <freddylikespotatos@gmail.com>
2022-09-24 20:18:13 -0500

    [WIP] Galactic Moon Festival Content

---
+++ sku.0/sys.server/compiled/game/script/event/halloween/pumpkin_smasher_object.java
@@ -0,0 +1,37 @@
+package script.event.halloween;
+
+import script.library.buff;
+import script.menu_info;
+import script.menu_info_types;
+import script.obj_id;
+import script.string_id;
+
+public class pumpkin_smasher_object extends script.base_script
+{
+    public pumpkin_smasher_object()
+    {
+    }
+    private static final String HALLOWEEN = "event/halloween";
+    public static final string_id SID_USE = new string_id(HALLOWEEN, "learn_song");
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
+    {
+        mi.addRootMenu(menu_info_types.ITEM_USE, SID_USE);
+        return SCRIPT_CONTINUE;
+    }
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
+    {
+        if (item == menu_info_types.SERVER_MENU1)
+        {
+            if (!buff.hasBuff(player, "event_halloween_pumpkin_pulper"))
+            {
+                buff.applyBuff(player, "event_halloween_pumpkin_pulper");
+            }
+            int randChance = rand(1,100);
+            if (randChance <= 90 ) {
+                broadcast(player, "You have found pumpkin puree!");
+            }
+            broadcast(player, "You've smashed the pumpkin.");
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
