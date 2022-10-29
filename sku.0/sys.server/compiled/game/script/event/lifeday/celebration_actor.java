fb13f16e4e4723129e1b51a3448ee13244fe9787
BubbaJoe <freddylikespotatos@gmail.com>
2022-10-28 23:39:49 -0500

    Start of Lifeday 22

---
+++ sku.0/sys.server/compiled/game/script/event/lifeday/celebration_actor.java
@@ -0,0 +1,52 @@
+/*
+
+Lifeday 2022
+
+Actor for the Lifeday Celebration. This actor is pure flavor. It does not affect gameplay.
+ */
+
+package script.event.lifeday;
+
+
+import script.location;
+import script.obj_id;
+import script.library.chat;
+import script.library.ai_lib;
+
+
+public class celebration_actor extends script.base_script
+{
+    public celebration_actor()
+    {
+    }
+    public static String[] actorChatter =
+            {
+                    "@@CHANGE_ME@@",
+                    "@@CHANGE_ME@@",
+                    "@@CHANGE_ME@@",
+            };
+
+    public int OnAttach(obj_id self) throws InterruptedException
+    {
+        setInvulnerable(self, true);
+        ai_lib.setDefaultCalmBehavior(self, ai_lib.BEHAVIOR_STOP);
+        setAnimationMood(self, "conversation");
+        chirp(self);
+        continueChirping(self);
+        return SCRIPT_CONTINUE;
+    }
+    public void chirp(obj_id self) throws InterruptedException
+    {
+        chat.chat(self, getRandomEntry(actorChatter));
+    }
+    public void continueChirping(obj_id self) throws InterruptedException
+    {
+        messageTo(self, "chirp", null, 360.0f, false);
+    }
+    public String getRandomEntry(String[] array) throws InterruptedException
+    {
+        int randomIndex = rand(0, array.length - 1);
+        String randomEntry = array[randomIndex];
+        return randomEntry;
+    }
+}
---
+++ sku.0/sys.server/compiled/game/script/event/lifeday/lifeday_gift_22.java
@@ -0,0 +1,14 @@
+package script.event.lifeday;
+
+import script.obj_id;
+
+public class lifeday_gift_22 extends script.base_script
+{
+    public lifeday_gift_22()
+    {
+    }
+    public int OnAttach(obj_id self) throws InterruptedException
+    {
+        return SCRIPT_CONTINUE;
+    }
+}
