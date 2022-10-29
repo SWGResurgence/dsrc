4b25a36fde4b88e62831551d4f79074df8ea38f0
BubbaJoe <freddylikespotatos@gmail.com>
2022-10-28 23:39:24 -0500

    Wardrobe container checks

--- sku.0/sys.server/compiled/game/script/item/wardrobe.java
+++ sku.0/sys.server/compiled/game/script/item/wardrobe.java
@@ -11,9 +11,30 @@ public class wardrobe extends script.base_script
     {
         if (!getTemplateName(item).contains("object/tangible/wearables/"))
         {
+            broadcast(transferer, "You can only put wearables in this container.");
             return SCRIPT_CONTINUE;
         }
-        setObjVar(item, "wardrobe", transferer);
+        if (!hasObjVar(item, "wardrobe.ownedBy"))
+        {
+            setObjVar(item, "wardrobe.ownedBy", transferer);
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public int OnAboutToLooseItem(obj_id self, obj_id destContainer, obj_id transferer, obj_id item) throws InterruptedException
+    {
+        if (!getTemplateName(item).contains("object/tangible/wearables/"))
+        {
+            broadcast(transferer, "You can only retrieve wearables from this container.");
+            return SCRIPT_CONTINUE;
+        }
+        if (hasObjVar(item, "wardrobe.ownedBy"))
+        {
+            if (getObjIdObjVar(item, "wardrobe.ownedBy") != transferer)
+            {
+                broadcast(transferer, "You can only retrieve your own wearables from this container.");
+                return SCRIPT_CONTINUE;
+            }
+        }
         return SCRIPT_CONTINUE;
     }
 }
