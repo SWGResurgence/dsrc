2eb5215aa57ef4ff883cd2bc1e67c78ae9ed48de
BubbaJoe <freddylikespotatos@gmail.com>
2022-10-31 10:44:42 -0500

    master_controller_boma.java formatting.

--- sku.0/sys.server/compiled/game/script/theme_park/dxun/master_controller_boma.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dxun/master_controller_boma.java
@@ -1,21 +1,25 @@
 package script.theme_park.dxun;
 
-import script.*;
-import script.library.hue;
-import script.library.colors;
-
 import script.color;
+import script.library.colors;
+import script.library.hue;
+import script.obj_id;
 
-public class master_controller_boma extends script.base_script {
-    public int OnAttach(obj_id self) throws InterruptedException {
-        setPalcolorCustomVarClosestColor(self, "/private/index_color_1", 23, 53, 11, 0);
-        hue.setColor(self, "/private/index_color_1", colors.DXUNGREEN);
-        return SCRIPT_CONTINUE;
-    }
-    public static void setPalcolorCustomVarClosestColor(obj_id target, String varPathName, color c) throws InterruptedException {
-        if (varPathName.startsWith("/")) {
+public class master_controller_boma extends script.base_script
+{
+    public static void setPalcolorCustomVarClosestColor(obj_id target, String varPathName, color c) throws InterruptedException
+    {
+        if (varPathName.startsWith("/"))
+        {
             varPathName = varPathName.substring(1);
         }
         setPalcolorCustomVarClosestColor(target, varPathName, c.getR(), c.getG(), c.getB(), c.getA());
+    }
+
+    public int OnAttach(obj_id self) throws InterruptedException
+    {
+        setPalcolorCustomVarClosestColor(self, "/private/index_color_1", 23, 53, 11, 0);
+        hue.setColor(self, "/private/index_color_1", colors.DXUNGREEN);
+        return SCRIPT_CONTINUE;
     }
 }
