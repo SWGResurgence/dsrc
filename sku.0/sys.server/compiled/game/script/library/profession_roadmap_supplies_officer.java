a499fab2736ea4e2451f58bc7d26ff48bfbf7adc
BubbaJoe <freddylikespotatos@gmail.com>
2022-10-31 13:44:10 -0500

    profession_roadmap_supplies_x reformatted.

--- sku.0/sys.server/compiled/game/script/library/profession_roadmap_supplies_officer.java
+++ sku.0/sys.server/compiled/game/script/library/profession_roadmap_supplies_officer.java
@@ -1,24 +1,18 @@
 package script.library;
 
-import script.*;
+import script.menu_info;
+import script.menu_info_types;
+import script.obj_id;
+import script.string_id;
+
 import java.util.HashSet;
 
-public class profession_roadmap_supplies_officer extends script.base_script {
+public class profession_roadmap_supplies_officer extends script.base_script
+{
     public static final String STF_FILE = "npe";
-    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException {
-        int mnu2 = mi.addRootMenu(menu_info_types.ITEM_USE, new string_id(STF_FILE, "claim_contents"));
-        return SCRIPT_CONTINUE;
-    }
-    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException {
-        if (item == menu_info_types.ITEM_USE) {
-            sendSystemMessage(player, new string_id(STF_FILE, "opened_crate"));
-            obj_id[] allTheArmor = grantOfficerRoadmapSupplies(player);
-            destroyObject(self);
-            return SCRIPT_CONTINUE;
-        }
-        return SCRIPT_CONTINUE;
-    }
-    public static obj_id[] grantOfficerRoadmapSupplies(obj_id player) throws InterruptedException {
+
+    public static obj_id[] grantOfficerRoadmapSupplies(obj_id player) throws InterruptedException
+    {
         obj_id pInv = utils.getInventoryContainer(player);
         HashSet theSet = new HashSet();
         theSet.add(static_item.createNewItemFunction("item_officer_backpack_01_02", pInv));
@@ -39,5 +33,23 @@ public class profession_roadmap_supplies_officer extends script.base_script {
         theSet.toArray(items);
         showLootBox(player, items);
         return items;
+    }
+
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
+    {
+        int mnu2 = mi.addRootMenu(menu_info_types.ITEM_USE, new string_id(STF_FILE, "claim_contents"));
+        return SCRIPT_CONTINUE;
+    }
+
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
+    {
+        if (item == menu_info_types.ITEM_USE)
+        {
+            sendSystemMessage(player, new string_id(STF_FILE, "opened_crate"));
+            obj_id[] allTheArmor = grantOfficerRoadmapSupplies(player);
+            destroyObject(self);
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
     }
 }
