ee1f3a3baf5c6a8ebc40b410b6410c622e678bff
BubbaJoe <freddylikespotatos@gmail.com>
2022-11-06 13:02:55 -0600

    City Decoration Converter
    GM's can use as many holographic decoys as they please. Because why not?

--- sku.0/sys.server/compiled/game/script/systems/city/city_decor_converter.java
+++ sku.0/sys.server/compiled/game/script/systems/city/city_decor_converter.java
@@ -1,5 +1,31 @@
 package script.systems.city;
 
-public class city_decor_converter
+import script.dictionary;
+import script.library.factions;
+import script.library.utils;
+import script.obj_id;
+
+public class city_decor_converter extends script.base_script
 {
+    public city_decor_converter()
+    {
+    }
+    public int OnAboutToLoseItem(obj_id self, obj_id destContainer, obj_id transferer, obj_id item) throws InterruptedException
+    {
+        if (isCrafted(item))
+        {
+            if (getTemplateName(item).contains("object/tangible/furniture/") || getTemplateName(item).contains("object/tangible/tarkin_custom/"))
+            {
+               attachScript(item, "systems.city.city_decor");
+               setName(item, " ");
+               //bad way to do this. @todo: add in jni call to hide name without hidding object.
+               return SCRIPT_CONTINUE;
+            }
+        }
+        else
+        {
+            broadcast(transferer, "You can only convert crafted items.");
+        }
+        return SCRIPT_CONTINUE;
+    }
 }
