b4feee38797a1979763499b3b57c83e4161e08de
Talisa Knight <talisaknight@gmail.com>
2022-11-24 10:42:36 -0600

    Script Refactoring.

--- sku.0/sys.server/compiled/game/script/developer/talisa/talisa_test.java
+++ sku.0/sys.server/compiled/game/script/developer/talisa/talisa_test.java
@@ -1011,6 +1011,20 @@ public class talisa_test extends script.base_script
                     sendSystemMessageTestingOnly(self, "shop_sign: " + shop_sign);
                     return SCRIPT_CONTINUE;
                 }
+				else if (priCommand.equalsIgnoreCase("clone_object"))
+                {
+                    obj_id target = utils.stringToObjId(secCommand);
+                    String[] scripts = getScriptList(target);
+                    obj_id cloned_item = utils.cloneObject(target,utils.getInventoryContainer(target));
+                    if (scripts.length > 0) {
+                        for (String script : scripts)
+                        {
+                            attachScript(cloned_item, script);
+                        }
+                    }
+                    setCrafter(cloned_item, self);
+                    return SCRIPT_CONTINUE;
+                }
                 else if (priCommand.equalsIgnoreCase("decrease_sign_skillmod") && !secCommand.equals(""))
                 {
                     int skillModAmount = getSkillStatMod(self, "special_sign_tcg_series3");
