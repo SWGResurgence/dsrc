318d898f1a53ba5df6bc46e1f6394bfc24306aa8
Talisa Knight <talisaknight@gmail.com>
2022-11-24 10:59:02 -0600

    Updated Treat Thief.

--- sku.0/sys.server/compiled/game/script/event/halloween/treat_thief.java
+++ sku.0/sys.server/compiled/game/script/event/halloween/treat_thief.java
@@ -23,7 +23,7 @@ public class treat_thief extends script.base_script {
     }
     public int OnCombatEntered(obj_id self, obj_id attacker, obj_id[] attackers, dictionary params) throws InterruptedException
     {
-        broadcast(self, "I AM HEREEEEE!");
+        sendSystemMessageGalaxyTestingOnly("[World Event] The Treat Thief has been spotted on " + toUpper(getCurrentSceneName(), 1));
         obj_id[] players = getPlayerCreaturesInRange(self, 64.0f);
         if (players == null || players.length == 0)
         {
@@ -32,23 +32,18 @@ public class treat_thief extends script.base_script {
         for (obj_id player : players) {
             addHate(self, player, 1);
         }
-        messageTo(self, "doTaunt", params, 120.0f, true);
-        messageTo(self, "doTauntSecondary", params, 360.0f, true);
         return SCRIPT_CONTINUE;
-    }
-    public void doTaunt(obj_id self, obj_id[] target, int damageModifier)
-    {
-        //do some cool attacks here
-    }
-    public void doTauntSecondary(obj_id self, obj_id[] targets)
-    {
-        //(possibly) do  some cool attacks here
-    }
-    public void handleLoot(obj_id self, obj_id player) throws InterruptedException {
-        broadcast(player, "Oh no! You've been robbed!");
     }
     public int OnCreatureDamaged(obj_id self, obj_id attacker, obj_id weapon, int[] damage) throws InterruptedException
     {
+        if (beast_lib.isBeast(attacker) || pet_lib.isPet(attacker))
+        {
+            attacker = getMaster(attacker);
+        }
+        if (!isPlayer(attacker))
+        {
+            return SCRIPT_CONTINUE;
+        }
         float max = getMaxHealth(self);
         float current = getHealth(self);
         float ratio = current / max;
@@ -69,26 +64,27 @@ public class treat_thief extends script.base_script {
                                     buff.applyBuff(self, "event_halloween_treat_thief", -1.0f);
                                 }
                             }
-                            spawnSidekick(self, "tusken_raider", "acid", 1.0f);
+                            spawnSidekick(self, "event_halloween_treat_thief_summon","frogBuff", 1.0f);
                             return SCRIPT_CONTINUE;
                         }
-                        spawnSidekick(self, "tusken_raider", "acid", 1.0f);
+                        chat.chat(self, "You're not getting away with this" + getName(attacker));
+                        spawnSidekick(self, "event_halloween_treat_thief_summon", "frogBuff", 1.0f);
                         return SCRIPT_CONTINUE;
                     }
-                    chat.chat(self, "All right, thats it! I'm calling in the big guns!");
-                    spawnSidekick(self, "", "acid", 1.0f);
+                    chat.chat(self, "Finally... I've been waiting for you " + getName(attacker));
+                    resurgence.createCircleSpawn(self, attacker,  "nuna", 20, 10.0f );
                     return SCRIPT_CONTINUE;
                 }
-                chat.chat(self, "Haha! I have your money!");
+                chat.chat(self, "Haha! I have your credits, " + getName(attacker) + "!");
                 broadcast(attacker, "You have been robbed!");
                 if (money.hasFunds(attacker, money.MT_TOTAL, 1000)) {
                     money.withdraw(attacker, 1000);
                 }
-                resurgence.pushPlayer(self, attacker,  -10.0f, 0.0f);
+                resurgence.pushPlayer(self, attacker,  -10.0f, 45.0f);
                 return SCRIPT_CONTINUE;
             }
-            chat.chat(self, "MY GOODIES!!");
-            resurgence.createCircleSpawn(self, self,  "tusken_raider", 16, 5.0f );
+            chat.chat(self, "Help me minions. My precious treats are being stolen!");
+            resurgence.createCircleSpawn(self, self,  "event_halloween_treat_thief_summon", 6, 5.0f );
             return SCRIPT_CONTINUE;
         }
         return SCRIPT_CONTINUE;
@@ -98,5 +94,17 @@ public class treat_thief extends script.base_script {
         setScale(sidekick, scale);
         buff.applyBuff(sidekick, buffName, -1.0f);
         attachScript(sidekick, "event_halloween_treat_thief");
+        chat.chat(sidekick, "Utinni!");
+    }
+    public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException
+    {
+        int lootChanc = rand(1, 100);
+        if (lootChanc <= 50)
+        {
+            obj_id loot = createObject("object/tangible/loot/creature_loot/collections/shared_treasure_map_01.iff", utils.getInventoryContainer(self), "");
+            setName(loot, "Treasure Map");
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
     }
 }
