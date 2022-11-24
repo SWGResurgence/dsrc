57230b6e283e7d642891d7e1e6aa68938e932653
Talisa Knight <talisaknight@gmail.com>
2022-11-24 10:49:37 -0600

    Updated Treat Thief.

--- sku.0/sys.server/compiled/game/script/event/halloween/treat_thief.java
+++ sku.0/sys.server/compiled/game/script/event/halloween/treat_thief.java
@@ -1,6 +1,9 @@
 package script.event.halloween;
-import script.*;
+
+import script.dictionary;
 import script.library.*;
+import script.menu_info;
+import script.obj_id;
 
 public class treat_thief extends script.base_script {
     public treat_thief() {
@@ -21,22 +24,79 @@ public class treat_thief extends script.base_script {
     public int OnCombatEntered(obj_id self, obj_id attacker, obj_id[] attackers, dictionary params) throws InterruptedException
     {
         broadcast(self, "I AM HEREEEEE!");
-        messageTo(self, "doAttackPrimary", params, 120.0f, true);
-        messageTo(self, "doAttackSecondary", params, 360.0f, true);
+        obj_id[] players = getPlayerCreaturesInRange(self, 64.0f);
+        if (players == null || players.length == 0)
+        {
+            return SCRIPT_CONTINUE;
+        }
+        for (obj_id player : players) {
+            addHate(self, player, 1);
+        }
+        messageTo(self, "doTaunt", params, 120.0f, true);
+        messageTo(self, "doTauntSecondary", params, 360.0f, true);
         return SCRIPT_CONTINUE;
     }
-    public void doAttackPrimary(obj_id self, obj_id[] target, int damageModifier)
+    public void doTaunt(obj_id self, obj_id[] target, int damageModifier)
     {
         //do some cool attacks here
     }
-    public void doAttackSecondary(obj_id self, obj_id[] targets)
+    public void doTauntSecondary(obj_id self, obj_id[] targets)
     {
         //(possibly) do  some cool attacks here
     }
     public void handleLoot(obj_id self, obj_id player) throws InterruptedException {
-        static_item.createNewItemFunction("test_item", getAppearanceInventory(player));
-        broadcast(player, "You have taken down the Treat Thief!");
-        broadcast(player, "Return to Shackel Emup on Tatooine.");
-
+        broadcast(player, "Oh no! You've been robbed!");
+    }
+    public int OnCreatureDamaged(obj_id self, obj_id attacker, obj_id weapon, int[] damage) throws InterruptedException
+    {
+        float max = getMaxHealth(self);
+        float current = getHealth(self);
+        float ratio = current / max;
+        if (ratio <= 0.9f)
+        {
+            if (ratio <= 0.75f)
+            {
+                if (ratio <= 0.6f)
+                {
+                    if (ratio <= 0.45f)
+                    {
+                        if (ratio <= 0.35f)
+                        {
+                            if (ratio <= 0.3f)
+                            {
+                                if (!buff.hasBuff(self, "event_halloween_treat_thief"))
+                                {
+                                    buff.applyBuff(self, "event_halloween_treat_thief", -1.0f);
+                                }
+                            }
+                            spawnSidekick(self, "tusken_raider", "acid", 1.0f);
+                            return SCRIPT_CONTINUE;
+                        }
+                        spawnSidekick(self, "tusken_raider", "acid", 1.0f);
+                        return SCRIPT_CONTINUE;
+                    }
+                    chat.chat(self, "All right, thats it! I'm calling in the big guns!");
+                    spawnSidekick(self, "", "acid", 1.0f);
+                    return SCRIPT_CONTINUE;
+                }
+                chat.chat(self, "Haha! I have your money!");
+                broadcast(attacker, "You have been robbed!");
+                if (money.hasFunds(attacker, money.MT_TOTAL, 1000)) {
+                    money.withdraw(attacker, 1000);
+                }
+                resurgence.pushPlayer(self, attacker,  -10.0f, 0.0f);
+                return SCRIPT_CONTINUE;
+            }
+            chat.chat(self, "MY GOODIES!!");
+            resurgence.createCircleSpawn(self, self,  "tusken_raider", 16, 5.0f );
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public void spawnSidekick(obj_id self, String creature, String buffName, float scale) throws InterruptedException {
+        obj_id sidekick = create.object(creature, getLocation(self));
+        setScale(sidekick, scale);
+        buff.applyBuff(sidekick, buffName, -1.0f);
+        attachScript(sidekick, "event_halloween_treat_thief");
     }
 }
