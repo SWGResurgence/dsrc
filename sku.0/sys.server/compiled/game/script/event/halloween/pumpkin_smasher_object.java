24abce9a13e4c6992d888ff6800b9b30d02a118f
BubbaJoe <freddylikespotatos@gmail.com>
2022-12-03 18:03:37 -0600

    Removed debug print from pumpkins
    Signed-off-by: BubbaJoe <freddylikespotatos@gmail.com>

--- sku.0/sys.server/compiled/game/script/event/halloween/pumpkin_smasher_object.java
+++ sku.0/sys.server/compiled/game/script/event/halloween/pumpkin_smasher_object.java
@@ -28,7 +28,6 @@ public class pumpkin_smasher_object extends script.base_script
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
     {
         mi.addRootMenu(menu_info_types.ITEM_USE, SID_USE);
-        broadcast(player, "SELECTED - OnObjectMenuRequest");
         return SCRIPT_CONTINUE;
     }
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
