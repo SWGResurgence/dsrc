2f682901dbaa85e8822b6ca1bd9ccf3310dfe201
BubbaJoe <freddylikespotatos@gmail.com>
2022-12-08 16:06:10 -0600

    Moved instantiated objects outside of the library folder.
    Signed-off-by: BubbaJoe <freddylikespotatos@gmail.com>

---
+++ sku.0/sys.server/compiled/game/script/item/vet_reward_token_box_large_02.java
@@ -0,0 +1,44 @@
+package script.item;
+
+import script.library.static_item;
+import script.library.utils;
+import script.menu_info;
+import script.menu_info_types;
+import script.obj_id;
+import script.string_id;
+
+import java.util.HashSet;
+
+public class vet_reward_token_box_large_02 extends script.base_script
+{
+    public static final String STF_FILE = "veteran_new";
+
+    public static obj_id[] grantVetTokens(obj_id player) throws InterruptedException
+    {
+        obj_id pInv = utils.getInventoryContainer(player);
+        HashSet theSet = new HashSet();
+        theSet.add(static_item.createNewItemFunction("item_vet_reward_token_01_01", pInv, 250));
+        obj_id[] items = new obj_id[theSet.size()];
+        theSet.toArray(items);
+        showLootBox(player, items);
+        return items;
+    }
+
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
+    {
+        int mnu2 = mi.addRootMenu(menu_info_types.ITEM_USE, new string_id(STF_FILE, "claim_vet_reward_tokens"));
+        return SCRIPT_CONTINUE;
+    }
+
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
+    {
+        if (item == menu_info_types.ITEM_USE)
+        {
+            sendSystemMessage(player, new string_id(STF_FILE, "opened_box"));
+            obj_id[] allTheArmor = grantVetTokens(player);
+            destroyObject(self);
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
