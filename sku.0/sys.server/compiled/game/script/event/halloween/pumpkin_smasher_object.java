88e5b4556c1f67094834e9c1c97de13622e50f2c
BubbaJoe <freddylikespotatos@gmail.com>
2022-12-08 20:29:56 -0600

    Updated pumpkin chance.
    Signed-off-by: BubbaJoe <freddylikespotatos@gmail.com>

--- sku.0/sys.server/compiled/game/script/event/halloween/pumpkin_smasher_object.java
+++ sku.0/sys.server/compiled/game/script/event/halloween/pumpkin_smasher_object.java
@@ -7,10 +7,7 @@
  */
 package script.event.halloween;
 
-import script.library.buff;
-import script.library.create;
-import script.library.static_item;
-import script.library.utils;
+import script.library.*;
 import script.menu_info;
 import script.menu_info_types;
 import script.obj_id;
@@ -44,14 +41,19 @@ public class pumpkin_smasher_object extends script.base_script
                 buff.applyBuff(player, "event_halloween_pumpkin_puree");
             }
             int coinChance = rand(1,100);
-            if (coinChance < 10 ) {
+            if (coinChance < 50 ) {
                 broadcast(player, "You find some strange objects inside this pumpkin.");
                 static_item.createNewItemFunction("item_event_halloween_coin", utils.getInventoryContainer(player), 5);
             }
             int mobChance = rand(1,100);
-            if (mobChance <= 6 ) {
+            if (mobChance <= 25 ) {
                 broadcast(player, "An angry worm seems to have been living inside this pumpkin!");
-                create.createCreature("angry_forage_worm", getLocation(player), true);
+                obj_id wormy = create.createCreature("angry_forage_worm", getLocation(player), true);
+                setName(wormy, "an uprooted worm");
+                setObjVar(wormy, "loot.lootTable", "treasure/treasure_81_90");
+                setObjVar(wormy, "loot.numItems", 5);
+                setScale(wormy, 1.5f);
+                showFlyText(wormy, new string_id("*- SLIME -*"), 12.0f, colors.GREEN);
             }
             playClientEffectLoc(player, "clienteffect/egg_hatch_01.cef", getLocation(player), 0.0f);
             int currentSmashed = getIntObjVar(player, PULPED_ITER_OBJVAR);
