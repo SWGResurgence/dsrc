d7d480e7f98cc9a488cf8f1c0e9360674d5e2e7e
BubbaJoe <freddylikespotatos@gmail.com>
2022-12-13 23:01:24 -0600

    Katiara's Toy radial string
    Signed-off-by: BubbaJoe <freddylikespotatos@gmail.com>

--- sku.0/sys.server/compiled/game/script/item/loot/toy.java
+++ sku.0/sys.server/compiled/game/script/item/loot/toy.java
@@ -47,7 +47,7 @@ public class toy extends script.base_script
     }
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
     {
-        mi.addRootMenu(menu_info_types.ITEM_USE, new string_id("spam", "use"));
+        mi.addRootMenu(menu_info_types.ITEM_USE, new string_id("Feel Lucky"));
         return SCRIPT_CONTINUE;
     }
     public int OnObjectMenuSelect(obj_id self, obj_id player, int mi) throws InterruptedException
