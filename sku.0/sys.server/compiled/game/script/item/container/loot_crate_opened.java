ffc3e8bf4ba4a7aec7fc230f974b1c3f8eec2daf
BubbaJoeX <jojojojo8@live.com>
2023-01-14 15:50:01 -0600

    Loot Crate scripts reversion
    Attach this script to whatever container that has a volume greater than 5, place any item you wish to make "discoverable"
    
    Then set the name to the crate. I use the cargo pod so I use "an ejected cargo pod" and place them near POIs/ROIs
    
    Signed-off-by: BubbaJoeX <jojojojo8@live.com>

--- sku.0/sys.server/compiled/game/script/item/container/loot_crate_opened.java
+++ sku.0/sys.server/compiled/game/script/item/container/loot_crate_opened.java
@@ -13,7 +13,16 @@ public class loot_crate_opened extends script.base_script
     }
     public int OnAboutToLoseItem(obj_id self, obj_id destContainer, obj_id transferer, obj_id item) throws InterruptedException
     {
-        debugServerConsoleMsg(self, "[SKYNET] Player " + transferer + " is attempting to move " + getName(item) + "(" + item +  ") to " + destContainer);
+        if (hasScript(item, "item.special.nomove"))
+        {
+            detachScript(item, "item.special.nomove");
+        }
+        if (hasObjVar(item, "noTrade"))
+        {
+            removeObjVar(item, "noTrade");
+        }
+        debugServerConsoleMsg(self, "\n[SKYNET] Player " + transferer + " is attempting to move " + getName(item) + "(" + item +  ") to " + destContainer + "\n");
+        broadcast(transferer, "This item will have it's No Trade tag reapplied 24 hours after it is moved.");
         return SCRIPT_CONTINUE;
     }
 }
