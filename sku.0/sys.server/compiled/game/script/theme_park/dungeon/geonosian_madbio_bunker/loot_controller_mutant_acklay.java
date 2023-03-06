d331094d6c7f28b5342b3db2eca005c92c353cd2
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-03-05 20:00:02 -0600

    Removed Loot Overrides for Geo Cave Bosses.

--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/geonosian_madbio_bunker/loot_controller_mutant_acklay.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/geonosian_madbio_bunker/loot_controller_mutant_acklay.java
@@ -6,8 +6,7 @@ import script.library.static_item;
 import script.library.utils;
 import script.obj_id;
 
-public class loot_controller_mutant_acklay extends script.base_script
-{
+public class loot_controller_mutant_acklay extends script.base_script {
     public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException
     {
         obj_id corpseInventory = utils.getInventoryContainer(self);
@@ -19,36 +18,6 @@ public class loot_controller_mutant_acklay extends script.base_script
         {
             return SCRIPT_CONTINUE;
         }
-        createMyLoot(self);
         return SCRIPT_CONTINUE;
-    }
-    public void createMyLoot(obj_id self) throws InterruptedException
-    {
-        obj_id corpseInventory = utils.getInventoryContainer(self);
-        if (corpseInventory == null)
-        {
-            return;
-        }
-        String mobType = ai_lib.getCreatureName(self);
-        if (mobType == null)
-        {
-            return;
-        }
-        int x = rand(1, 100);
-        if (x < 51){  // 50% Drop Chance
-            static_item.createNewItemFunction("item_scorpion_kliknik_dna", corpseInventory);
-        }
-        if (x < 26){  // 25% Drop Chance
-            static_item.createNewItemFunction("item_tcg_loot_reward_series4_geonosian_speeder_02_01", corpseInventory);
-        }
-        if (x < 2){  // 01% Drop Chance
-            static_item.createNewItemFunction("item_tcg_loot_reward_series2_barn", corpseInventory);
-        }
-
-        /*String myLoot1 = "";
-        String myLoot2 = "object/tangible/wearables/cybernetic/s02/cybernetic_s02_arm_r.iff";
-        createObject(myLoot1, corpseInventory, "");
-        createObject(myLoot2, corpseInventory, "");*/ 
-        return;
     }
 }
--- sku.0/sys.server/compiled/game/script/theme_park/dungeon/geonosian_madbio_bunker/loot_controller_nexu.java
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/geonosian_madbio_bunker/loot_controller_nexu.java
@@ -19,35 +19,6 @@ public class loot_controller_nexu extends script.base_script
         {
             return SCRIPT_CONTINUE;
         }
-        createMyLoot(self);
         return SCRIPT_CONTINUE;
-    }
-    public void createMyLoot(obj_id self) throws InterruptedException
-    {
-        obj_id corpseInventory = utils.getInventoryContainer(self);
-        if (corpseInventory == null)
-        {
-            return;
-        }
-        String mobType = ai_lib.getCreatureName(self);
-        if (mobType == null)
-        {
-            return;
-        }
-        int x = rand(1, 100);
-        if (x < 76) {      // 75% Drop Rate: Geonosian Solar Sail ITV
-            static_item.createNewItemFunction("item_tcg_loot_reward_series4_home_itv_02_01", corpseInventory);
-            if (x < 51) {  // 50% Drop Chance: Nexu DNA Core
-                static_item.createNewItemFunction("item_cs_dna_nexu", corpseInventory);
-            }
-            if (x < 26) {  // 25% Drop Chance: Auto Feeder
-            static_item.createNewItemFunction("item_tcg_loot_reward_series6_auto_feeder", corpseInventory);
-            }
-        }
-        /*String myLoot1 = "object/tangible/tcg/series4/instant_travel_terminal_home.iff";
-        String myLoot2 = "object/tangible/wearables/cybernetic/s02/cybernetic_s02_arm_r.iff";
-        createObject(myLoot1, corpseInventory, "");
-        createObject(myLoot2, corpseInventory, "");*/
-        return;
     }
 }
