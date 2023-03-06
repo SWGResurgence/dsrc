b26e19bca9acabde8b48e454122b53c51cf77aa7
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-03-05 20:06:00 -0600

    Minor Code Cleanup.

---
+++ sku.0/sys.server/compiled/game/script/theme_park/dungeon/geonosian_madbio_bunker/master_controller_nexu.java
@@ -0,0 +1,18 @@
+package script.theme_park.dungeon.geonosian_madbio_bunker;
+
+import script.dictionary;
+import script.library.*;
+import script.obj_id;
+
+public class master_controller_nexu extends script.base_script {
+    public int aiCorpsePrepared(obj_id self, dictionary params) throws InterruptedException {
+        obj_id corpseInventory = utils.getInventoryContainer(self);
+        if (corpseInventory == null)  {
+            return SCRIPT_CONTINUE;
+        }
+        if (!isIdValid(self))  {
+            return SCRIPT_CONTINUE;
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
