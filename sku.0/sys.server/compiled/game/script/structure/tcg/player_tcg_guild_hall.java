411a8ac5134d0dbdbcbfe15283a56fe8ca06f89d
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-03-25 16:24:36 -0500

    Added Buff to TCG: AT-AT House.

--- sku.0/sys.server/compiled/game/script/structure/tcg/player_tcg_guild_hall.java
+++ sku.0/sys.server/compiled/game/script/structure/tcg/player_tcg_guild_hall.java
@@ -4,9 +4,9 @@ import script.base_script;
 import script.library.buff;
 import script.obj_id;
 
-public class player_tcg_guild_halls extends base_script {
+public class player_tcg_guild_hall extends base_script {
     public int OnReceivedItem(obj_id self, obj_id srcContainer, obj_id transferer, obj_id item) throws InterruptedException {
-        buff.applyBuff(item, "tcg_guild_halls_buff");
+        buff.applyBuff(item, "tcg_guild_hall_buff");
     return SCRIPT_CONTINUE;
     }
 }
