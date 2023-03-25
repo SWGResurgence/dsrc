08e45a2477b796a9d019a1242969f01abc2dc38e
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-03-25 14:05:09 -0500

    Added XP Buff to TCG: Yoda's Hut.

---
+++ sku.0/sys.server/compiled/game/script/structure/tcg/player_tcg_yoda_hut.java
@@ -0,0 +1,12 @@
+package script.structure.tcg;
+
+import script.base_script;
+import script.library.buff;
+import script.obj_id;
+
+public class player_tcg_yoda_hut extends base_script {
+    public int OnReceivedItem(obj_id self, obj_id srcContainer, obj_id transferer, obj_id item) throws InterruptedException {
+        buff.applyBuff(item, "tcg_yoda_hut_buff");
+    return SCRIPT_CONTINUE;
+    }
+}
