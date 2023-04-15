aed708f56c02101f006d84bf413186dcc6311afd
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-04-14 23:24:09 -0500

    Fixed Bug with Space GCW Not Awarding Tokens.

--- sku.0/sys.server/compiled/game/script/systems/gcw/space/support_ship.java
+++ sku.0/sys.server/compiled/game/script/systems/gcw/space/support_ship.java
@@ -5,17 +5,14 @@ import script.library.space_combat;
 import script.library.space_utils;
 import script.obj_id;
 
-public class support_ship extends script.space.combat.combat_ship
-{
-    public int OnDestroy(obj_id self) throws InterruptedException
-    {
+public class support_ship extends script.space.combat.combat_ship {
+    public int OnDestroy(obj_id self) throws InterruptedException {
         dictionary dict = space_combat.getKillPercentages(self);
         obj_id playerShip = dict.getObjId("objWinner");
 
         obj_id motherShip = getObjIdObjVar(self, "motherShip");
 
-        if (space_utils.isPlayerControlledShip(playerShip))
-        {
+        if (space_utils.isPlayerControlledShip(playerShip)){
             obj_id player = getOwner(playerShip);
 
             obj_id spawner = getObjIdObjVar(motherShip, "spawner");
@@ -26,30 +23,23 @@ public class support_ship extends script.space.combat.combat_ship
             if (getIntObjVar(controller, "space_gcw." + spawner + ".active") == 0)
                 return SCRIPT_CONTINUE;
 
-            if (space_utils.isPobType(playerShip))
-            {
-                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP))
-                {
+            if (space_utils.isPobType(playerShip)) {
+                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP)) {
                     if (!(pvpGetType(playerShip) == PVPTYPE_DECLARED))
                         return SCRIPT_CONTINUE;
                 }
                 LOG("space_gcw", "Ship \"" + playerShip + "\" (" + playerShip + ") has qualified for space battle " + battleId + " by destroying a support ship.");
                 setObjVar(spawner, "space_gcw.pob.participant." + battleId + "." + playerShip, space_utils.getAllPlayersInShip(playerShip));
-            }
-            else if (getShipChassisType(playerShip).startsWith("player_gunship"))
-            {
-                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP))
-                {
+            }  else if (getShipChassisType(playerShip).startsWith("player_gunship")) {
+                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP)) {
+                	
                     if (!(pvpGetType(playerShip) == PVPTYPE_DECLARED))
                         return SCRIPT_CONTINUE;
                 }
                 LOG("space_gcw", "Ship \"" + playerShip + "\" (" + playerShip + ") has qualified for space battle " + battleId + " by destroying a support ship.");
                 setObjVar(spawner, "space_gcw.gunship.participant." + battleId + "." + playerShip, space_utils.getAllPlayersInShip(playerShip));
-            }
-            else
-            {
-                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP))
-                {
+            } else {
+                if (battleType.equals(battle_spawner.BATTLE_TYPE_PVP)) {
                     if (!(pvpGetType(playerShip) == PVPTYPE_DECLARED))
                         return SCRIPT_CONTINUE;
                 }
