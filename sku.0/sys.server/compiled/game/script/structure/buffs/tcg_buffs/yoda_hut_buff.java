56e8885e5fec22e7b17937de9c6b04f24282d34a
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-04-16 21:29:43 -0500

    Part of Previous Commit.

--- sku.0/sys.server/compiled/game/script/structure/buffs/tcg_buffs/yoda_hut_buff.java
+++ sku.0/sys.server/compiled/game/script/structure/buffs/tcg_buffs/yoda_hut_buff.java
@@ -2,11 +2,20 @@ package script.structure.buffs.tcg_buffs;
 
 import script.base_script;
 import script.library.buff;
+import script.library.utils;
 import script.obj_id;
 
-public class yoda_hutt_buff extends base_script {
+public class yoda_hut_buff extends base_script {
     public int OnReceivedItem(obj_id self, obj_id srcContainer, obj_id transferer, obj_id item) throws InterruptedException {
-        buff.applyBuff(item, "tcg_yoda_hut_buff");
+        if (isPlayer(item) && utils.isProfession(item, utils.TRADER)) {
+            buff.applyBuff(item, "tcg_yoda_hut_buff");
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public int OnAboutToLoseItem(obj_id self, obj_id srcContainer, obj_id transferer, obj_id item) throws InterruptedException {
+        if (isPlayer(item) && buff.hasBuff(item, "tcg_yoda_hut_buff")) {
+            buff.removeBuff(item, "tcg_yoda_hut_buff");
+        }
         return SCRIPT_CONTINUE;
     }
 }
