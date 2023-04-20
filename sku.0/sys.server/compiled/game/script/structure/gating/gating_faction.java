0fd7322e4cd95eff7c3bd0a7670e6a21a5db8c7a
Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>
2023-04-19 23:50:49 -0500

    Generic gating scripts.
    Signed-off-by: Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>

---
+++ sku.0/sys.server/compiled/game/script/structure/gating/gating_faction.java
@@ -0,0 +1,62 @@
+package script.structure.gating;/*
+@Filename: script.structure.gating.
+@Author: BubbaJoeX
+@Purpose: Restricts entry if player is not of the correct faction. [Rebel, Imperial, Neutral]
+*/
+
+import script.obj_id;
+
+import static script.library.factions.isImperial;
+import static script.library.factions.isRebel;
+
+public class gating_faction extends script.base_script
+{
+    public int OnAttach(obj_id self)
+    {
+        return SCRIPT_CONTINUE;
+    }
+
+    public int OnInitialize(obj_id self)
+    {
+        return SCRIPT_CONTINUE;
+    }
+
+    public int OnAboutToReceiveItem(obj_id self, obj_id srcContainer, obj_id transferer, obj_id item) throws InterruptedException
+    {
+        if (isPlayer(item))
+        {
+            String gatingFaction = getStringObjVar(self, "gating.faction");
+            if (gatingFaction != null && gatingFaction.length() > 0)
+            {
+                switch (gatingFaction)
+                {
+                    case "rebel":
+                    {
+                        if (!isRebel(item))
+                        {
+                            broadcast(transferer, "You must be a Rebel to enter this structure.");
+                            return SCRIPT_OVERRIDE;
+                        }
+                    }
+                    case "imperial":
+                    {
+                        if (!isImperial(item))
+                        {
+                            broadcast(transferer, "You must be an Imperial to enter this structure.");
+                            return SCRIPT_OVERRIDE;
+                        }
+                    }
+                    case "neutral":
+                    {
+                        if (isRebel(item) || isImperial(item))
+                        {
+                            broadcast(transferer, "You must be Neutral to enter this structure.");
+                            return SCRIPT_OVERRIDE;
+                        }
+                    }
+                }
+            }
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
