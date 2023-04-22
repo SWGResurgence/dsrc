f20bf906e5a9ae6faf271a8846d009ae36d285b0
Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>
2023-04-22 13:29:43 -0500

    registerWithLocation will now notify the feed with a waypoint and who is servicing.
    Signed-off-by: Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>

--- sku.0/sys.server/compiled/game/script/player/cmd/register.java
+++ sku.0/sys.server/compiled/game/script/player/cmd/register.java
@@ -1,5 +1,6 @@
 package script.player.cmd;
 
+import script.library.locations;
 import script.library.planetary_map;
 import script.library.utils;
 import script.map_location;
@@ -84,6 +85,7 @@ public class register extends script.base_script
                 utils.setBatchScriptVar(updateTarget, "registrants", registrants);
                 sendSystemMessage(self, SID_SUCCESS_REGISTER);
                 utils.setScriptVar(self, "registerWithLocation", updateTarget);
+                notifyServerEvents(getName(self) + " has started enchancement services within " + locations.getCityName(getLocation(self)) + "\n`/wp " + getLocation(self).x + " " + getLocation(self).z + " " + getLocation(self).y + " " + getLocation(self).area + "`");
             }
         }
         else 
