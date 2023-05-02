bdca063e152af789234dcd5a755465ff70643a63
Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>
2023-05-02 15:28:08 -0500

    Allow the choice of corellia/tatooine/naboo for the city care package.

--- sku.0/sys.server/compiled/game/script/item/trader_care_package_cities.java
+++ sku.0/sys.server/compiled/game/script/item/trader_care_package_cities.java
@@ -1,30 +1,30 @@
 package script.item;
 
+import script.*;
 import script.library.static_item;
+import script.library.sui;
 import script.library.utils;
-import script.menu_info;
-import script.menu_info_types;
-import script.obj_id;
-import script.string_id;
 
 import java.util.HashSet;
 
 public class trader_care_package_cities extends script.base_script
 {
-    public static final String STF_FILE = "npe";
-    public static obj_id[] grantTraderCarePackageCities(obj_id player) throws InterruptedException
+    public static String[] PLANETS = {
+            "Corellia",
+            "Naboo",
+            "Tatooine"
+    };
+    public static obj_id[] grantTraderCarePackageCities(obj_id player, String planet) throws InterruptedException
     {
         obj_id pInv = utils.getInventoryContainer(player);
         HashSet theSet = new HashSet<>();
-
-        createObject("object/tangible/deed/guild_deed/cityhall_naboo_deed.iff", pInv, "");
-        createObject("object/tangible/deed/city_deed/bank_naboo_deed.iff", pInv, "");
-        createObject("object/tangible/deed/player_house_deed/naboo_house_small_deed.iff", pInv, "");
-        createObject("object/tangible/deed/player_house_deed/naboo_house_small_deed.iff", pInv, "");
-        createObject("object/tangible/deed/player_house_deed/naboo_house_small_deed.iff", pInv, "");
-        createObject("object/tangible/deed/player_house_deed/naboo_house_small_deed.iff", pInv, "");
-        createObject("object/tangible/deed/player_house_deed/naboo_house_small_deed.iff", pInv, "");
-
+        createObject("object/tangible/deed/city_deed/cityhall_" + planet + "_deed.iff", pInv, "");
+        createObject("object/tangible/deed/city_deed/bank_" + planet + "_deed.iff", pInv, "");
+        createObject("object/tangible/deed/player_house_deed/" + planet + "_house_small_deed.iff", pInv, "");
+        createObject("object/tangible/deed/player_house_deed/" + planet + "_house_small_deed.iff", pInv, "");
+        createObject("object/tangible/deed/player_house_deed/" + planet + "_house_small_deed.iff", pInv, "");
+        createObject("object/tangible/deed/player_house_deed/" + planet + "_house_small_deed.iff", pInv, "");
+        createObject("object/tangible/deed/player_house_deed/" + planet + "_house_small_deed.iff", pInv, "");
         obj_id[] items= new obj_id[theSet.size()];
         theSet.toArray(items);
         showLootBox(player, items);
@@ -32,18 +32,30 @@ public class trader_care_package_cities extends script.base_script
     }
     public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
     {
-        int mnu2 = mi.addRootMenu(menu_info_types.ITEM_USE, new string_id(STF_FILE, "redeem_care_package"));
+        mi.addRootMenu(menu_info_types.ITEM_USE, unlocalized("Use Care Package"));
         return SCRIPT_CONTINUE;
     }
     public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
     {
         if (item == menu_info_types.ITEM_USE)
         {
-            sendSystemMessage(player, new string_id(STF_FILE, "redeemed_care_package"));
-            obj_id[] allTheArmor = grantTraderCarePackageCities(player);
-            destroyObject(self);
+            sui.listbox(self, player, "Select a which planet you'd like for a City Hall Deed, and four Small House Deed", sui.OK_CANCEL, "Trader Care Package: Cities", PLANETS, "handleTargetLock", true, false);
             return SCRIPT_CONTINUE;
         }
+        return SCRIPT_CONTINUE;
+    }
+    public int handleCarePackageCities(obj_id self, dictionary params) throws InterruptedException
+    {
+        obj_id player = sui.getPlayerId(params);
+        int idx = sui.getListboxSelectedRow(params);
+        if (idx < 0)
+        {
+            return SCRIPT_CONTINUE;
+        }
+        String planet = PLANETS[idx];
+        planet.toLowerCase();
+        grantTraderCarePackageCities(player, planet);
+        destroyObject(self);
         return SCRIPT_CONTINUE;
     }
 }
