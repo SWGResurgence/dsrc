0252128d8a7ea026410b48353a683da328fff070
Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>
2023-05-04 19:04:18 -0500

    Wrong values for deeds
    Signed-off-by: Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>

--- sku.0/sys.server/compiled/game/script/systems/veteran_reward/resource.java
+++ sku.0/sys.server/compiled/game/script/systems/veteran_reward/resource.java
@@ -10,7 +10,7 @@ import java.util.Vector;
 
 public class resource extends script.base_script
 {
-    public static final int RESOURCE_AMOUNT = 20000;
+    public static final int RESOURCE_AMOUNT = 50000;
     public static final String ROOT_RESOURCE_CLASS = "resource";
     public static final String ROOT_ORGANIC_CLASS = "organic";
     public static final String ROOT_INORGANIC_CLASS = "inorganic";
