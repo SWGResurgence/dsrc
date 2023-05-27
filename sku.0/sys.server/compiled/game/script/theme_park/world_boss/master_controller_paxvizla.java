78a327c0d2720c5d108e96ced7acd70558d1f39e
Cesilia Targaryen <cesilia.foster@gmail.com>
2023-05-27 14:17:53 -0500

    Pax Vizla Has Been Given A Mechanic.

--- sku.0/sys.server/compiled/game/script/theme_park/world_boss/master_controller_paxvizla.java
+++ sku.0/sys.server/compiled/game/script/theme_park/world_boss/master_controller_paxvizla.java
@@ -20,7 +20,7 @@ public class master_controller_paxvizla extends script.base_script
 
     public int OnInitialize(obj_id self) throws InterruptedException
     {
-        obj_id tatooine = getPlanetByName("tatooine");
+        obj_id tatooine = getPlanetByName("dxun");
         if (hasObjVar(tatooine, "dungeon_finder.world_boss.pax"))
         {
             removeObjVar(tatooine, "dungeon_finder.world_boss.pax");
@@ -31,7 +31,7 @@ public class master_controller_paxvizla extends script.base_script
 
     public int OnDestroy(obj_id self) throws InterruptedException
     {
-        obj_id tatooine = getPlanetByName("tatooine");
+        obj_id tatooine = getPlanetByName("dxun");
         if (hasObjVar(tatooine, "dungeon_finder.world_boss.pax"))
         {
             removeObjVar(tatooine, "dungeon_finder.world_boss.pax");
@@ -83,9 +83,10 @@ public class master_controller_paxvizla extends script.base_script
                 chat.chat(self, "I will not be challenged in such uncivil ways!");
                 for (obj_id who : players)
                 {
-                    broadcast(who, "Pax has lost his enhancements. Strike now!");
+                    broadcast(who, "Pax has lost his enhancements, he has summoned reinforcements from House Vizla. Strike now before you lose the tactical advantage! ");
                 }
                 buff.removeAllBuffs(self);
+                resurgence.createCircleSpawn(self, self, "clan_vizla_mandalorian", 8, 12);
                 utils.setScriptVar(self, "hasSpawned", 1);
                 return SCRIPT_CONTINUE;
             }
@@ -107,8 +108,9 @@ public class master_controller_paxvizla extends script.base_script
                 chat.chat(self, MAND_MSGS[rand(0, MAND_MSGS.length - 1)]);
                 for (obj_id who : players)
                 {
-                    broadcast(who, "The most recent attack from " + getFirstName(attacker) + " has enraged Pax Vizla, causing him to increase his focus.");
+                    broadcast(who, "The most recent attack from " + getFirstName(attacker) + " has enraged Pax Vizla, causing him to increase his focus. He has summoned reinforcements from a nearby Mandalorian Patrol.");
                 }
+                resurgence.createCircleSpawn(self, self, "clan_vizla_mandalorian_patrol", 4, 12);
                 utils.setScriptVar(self, "hasDisarmed", 1);
             }
         }
@@ -119,10 +121,11 @@ public class master_controller_paxvizla extends script.base_script
                 buff.removeAllBuffs(self);
                 for (obj_id who : players)
                 {
-                    broadcast(who, "Pax Vizla has entered his last stand!");
+                    broadcast(who, "Pax Vizla has entered his last stand, and he has summoned reinforcements from his personal guard!");
                 }
                 chat.chat(self, "This Is The Way.");
                 buff.applyBuff(self, "crystal_buff", 30, 10);
+                resurgence.createCircleSpawn(self, self, "clan_vizla_mandalorian_guard", 2, 6);
                 utils.setScriptVar(self, "hasLastStand", 1);
             }
         }
