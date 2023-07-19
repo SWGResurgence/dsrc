d4c9dd364a2602ac1859503a39fe7b12630c0572
Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>
2023-07-19 16:40:34 -0500

    Additional Logging
    Signed-off-by: Bubba Joe <89060381+BubbaJoeX@users.noreply.github.com>

--- sku.0/sys.server/compiled/game/script/player/player_resurgence.java
+++ sku.0/sys.server/compiled/game/script/player/player_resurgence.java
@@ -24,6 +24,7 @@ public class player_resurgence extends script.base_script
 {
     public boolean requireEntBuffRecycle = false;
     public boolean restoredContent = false;
+    public boolean LOGGING = true;
 
     public int OnAttach(obj_id self)
     {
@@ -75,7 +76,7 @@ public class player_resurgence extends script.base_script
         decrementPlayerCount(self);
         if (resurgence.isEthereal(self))
         {
-            resurgence.logEtherealAction(self, "Logging out (or switching zones) at " + getCalendarTimeStringLocal_YYYYMMDDHHMMSS(getGameTime()) + " near location " + getLocation(self));
+            resurgence.logEtherealAction(self, "Logging out (or switching zones) at " + getCalendarTimeStringLocal_YYYYMMDDHHMMSS(getGameTime()) + " near location " + getLocation(self).toReadableFormat(true));
         }
         return SCRIPT_CONTINUE;
     }
@@ -390,6 +391,32 @@ public class player_resurgence extends script.base_script
         else
         {
             broadcast(self, "You must have two targets selected (mouse-over and target) to use this command.");
+        }
+        return SCRIPT_CONTINUE;
+    }
+
+    public int cmdTell(obj_id self, obj_id target, String params, float defaultTime) throws InterruptedException
+    {
+        if (LOGGING)
+        {
+            //String s_outputfile = "/home/swg/swg-main/exe/linux/logs/im.log";
+            StringTokenizer parser = new StringTokenizer(params);
+            String who = parser.nextToken();
+            String message = "";
+            while (parser.hasMoreTokens())
+            {
+                message = parser.nextToken() + " ";
+            }
+            String finalizedMessage = message;
+            LOG("InstantMessage", "[" + getFirstName(self) + "] to [" + toUpper(who, 0) + "]: " + finalizedMessage);
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public int OnSpeaking(obj_id self, String text)
+    {
+        if (LOGGING)
+        {
+            LOG("Spatial", "[" + getFirstName(self) + "] said: " + text);
         }
         return SCRIPT_CONTINUE;
     }
